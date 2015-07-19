68d2b2d83702aabb13228a352ba448e375e971de
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-19 23:13:18 +0100

    Create Fizz.lua

---
+++ Fizz.lua
@@ -0,0 +1,25 @@
+require('Common')
+function AfterObjectLoopEvent(myHer0)
+    myHero = myHer0
+    myHeroPos = GetOrigin(myHero)
+		DrawText("Remember that this is still in alpha version.",24,0,0,0xffff0000);
+	
+	local target = GetCurrentTarget()
+	if KeyIsDown(0x20) then 
+	    if ValidTarget(target, 1275) then
+		    local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1875,250,1275,55,true,true)
+            if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 then
+            CastSkillShot(_R,RPred.PredPos.x,Rred.PredPos.y,RPred.PredPos.z)
+            end
+			if CanUseSpell(myHero, _W) == READY and GetDistance(GetOrigin(target), GetOrigin(myHero)) < 200*200 then
+				CastTargetSpell(myHero, _W)
+			end
+			if CanUseSpell(myHero, _Q) == READY and GetDistance(GetOrigin(target), GetOrigin(myHero)) < 550*550 then
+				CastTargetSpell(target, _Q)
+			end
+			if CanUseSpell(myHero, _E) == READY and GetDistance(GetOrigin(target), GetOrigin(myHero)) < 900*900 then
+				CastTargetSpell(target, _E)
+			end
+	  	end
+	end	
+end
