2fd80d1af613354de96249e37c96dd032ffc2b25
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-20 00:27:30 +0100

    Update AutoIgnite.lua

--- AutoIgnite.lua
+++ AutoIgnite.lua
@@ -1,13 +1,12 @@
-require('Common')
-local Ignite = (GetCastName(myHero,SUMMONER_1):lower():find("summonerdot") and SUMMONER_1 or (GetCastName(myHero,SUMMONER_2):lower():find("summonerdot") and SUMMONER_2 or nil))
-function AfterObjectLoopEvent(myHer0)
-    myHero = myHer0
-    myHeroPos = GetOrigin(myHero)
-        if ValidTarget(target, 600) then
-		    if Ignite and CanUseSpell(myHero, Ignite) and WillIgniteKill(target) then 
-            CastTargetSpell(target, Ignite)
-            end
-		end 
+Ignite = (GetCastName(myHero,SUMMONER_1):lower():find("summonerdot") and SUMMONER_1 or (GetCastName(myHero,SUMMONER_2):lower():find("summonerdot") and SUMMONER_2 or nil))
+function AutoIgnite()
+        for _,target in pairs(GetEnemyHeroes()) do
+                if ValidTarget(target, 600) then
+                        if Ignite and CanUseSpell(GetMyHero(), Ignite) and WillIgniteKill(target) then
+                                CastTargetSpell(target, Ignite)
+                        end
+                end
+        end
 end
  
 function WillIgniteKill(target)
