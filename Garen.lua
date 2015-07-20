a5537678f0801ef64f37cf67d122783997a437d4
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-20 16:14:07 +0100

    Update Garen.lua

--- Garen.lua
+++ Garen.lua
@@ -10,31 +10,35 @@ AddKey("LastHit", "Do LastHit", string.byte("X"))
 AddKey("LaneClear", "Do LaneClear", string.byte("V"))
 
 function AfterObjectLoopEvent(myHero)
+    myHero = myHer0
+    myHeroPos = GetOrigin(myHero)
 		IWalk()
 		DrawMenu()
-	        AutoIgnite()
-            if GetKeyValue("Combo") then
-            local Target = GetTarget(1000)
-		  if ValidTarget(unit, 1000) then
+	    AutoIgnite()
+        if GetKeyValue("Combo") then
+          local target = GetCurrentTarget()
+		  if ValidTarget(target, 1000) then
 				
-	                if GetButtonValue("E") then
-			 if CanUseSpell(myHero, _E) == READY and GetDistance(target, myHero) < 300 then
+	        if GetButtonValue("E") then
+			 if CanUseSpell(myHero, _E) == READY and GetDistance(GetOrigin(target), GetOrigin(myHero)) < 300*300 then
 				CastTargetSpell(target, _E)
 			 end
 			end
 			
-		        if GetButtonValue("R") then
-			 if CanUseSpell(myHero, _R) == READY and CalcDamage(myHero, Target, 0, GetRdmg) > GetCurrentHP(Target) then
-                         CastTargetSpell(Target, _R)
+		    if GetButtonValue("R") then
+			 if CanUseSpell(myHero, _R) == READY and CalcDamage(myHero, unit, 0, GetRdmg()) > GetCurrentHP(Target) then
+             CastTargetSpell(Target, _R)
 			 end
-		    end
-          end  
-        end	
+            end  
+          end	
+        end
 end
 
 function GetRdmg()
+local target = GetCurrentTarget()
 local spelllevel = GetCastLevel(MyHero,_R);
-local missingHealth = GetMaxhealth(target)-GetCurrentHealth(target);
-local MagicReduction = GetMagicRediction(target) / 100;
+local missingHealth = GetMaxHP(target)-GetCurrentHP(target);
+local MR = GetMagicResist(target);
+local MagicReduction =MR/(MR+100);
 local dmg=( (8-spelllevel)*0.5*missingHealth + 175*spelllevel) * 1- MagicReduction
 end
