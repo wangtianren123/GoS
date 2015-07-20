c937819941eb585255e2458a449c7b0dbe7a1d0c
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-20 16:37:13 +0100

    Update Garen.lua

--- Garen.lua
+++ Garen.lua
@@ -20,10 +20,10 @@ function AfterObjectLoopEvent(myHero)
           if ValidTarget(target, 1000) then
 				
 	        if GetButtonValue("E") then
-			 if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 300) then
-				CastTargetSpell(myHero, _E)
-			 end
-			end
+		 if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 300) then
+		 CastTargetSpell(myHero, _E)
+		 end
+		end
 			
 	        if GetButtonValue("R") then
 		 if CanUseSpell(myHero, _R) == READY and CalcDamage(myHero, unit, 0, GetRdmg()) > GetCurrentHP(Target) and IsInDistance(target, 650) then
