78902bc773b3c45b012e6507f1115f6572760d12
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-27 15:25:29 +0100

    Update Fizz.lua

--- Fizz.lua
+++ Fizz.lua
@@ -1,39 +1,33 @@
-AddInfo("Fizz", "Fizz")
-AddButton("Q", "Use Q", true)
-AddButton("W", "Use W", true)
-AddButton("E", "Use E", true)
-AddButton("R", "Use R", true)
+Config = scriptConfig("Fizz", "Fizz:")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
 
 OnLoop(function(myHero)
 		
-		if GetKeyValue("Combo") then
-		 local target = GetTarget(1275, DAMAGE_MAGIC)
+        if IWalkConfig.Combo then
+	       local target = GetTarget(1275, DAMAGE_MAGIC)
 	                if ValidTarget(target, 1275) then
-			       local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1350,250,1275,120,false,true)
-		               if GetButtonValue("R") then
-                                if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 then
+	                	
+	                	
+			        local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1350,250,1275,120,false,true)
+                                if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and Config.R then
                                 CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 	                        end
-                               end
 				
-				if GetButtonValue("W") then
-				 if CanUseSpell(myHero, _W) == READY and IsInDistance(target, 300) then
-				 CastTargetSpell(myHero, _W)
-				 end
+				if CanUseSpell(myHero, _W) == READY and IsInDistance(target, 300) and Config.W then
+				CastTargetSpell(myHero, _W)
 				end
 				
-				if GetButtonValue("Q") then
-				 if CanUseSpell(myHero, _Q) == READY and IsInDistance(target, 550) then
-				 CastTargetSpell(target, _Q)
-			         end
-				end
+				if CanUseSpell(myHero, _Q) == READY and IsInDistance(target, 550) and Config.Q then
+			        CastTargetSpell(target, _Q)
+			        end
 				 
-				if GetButtonValue("E") then
-			         if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 800) then
+			         if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 800) and Config.E then
 				 CastTargetSpell(target, _E)
 			         end
-				end
 				
                         end
-                end
+        end
 end)
