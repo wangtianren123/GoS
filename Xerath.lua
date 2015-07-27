2756515a5f82e426e89c03dc8d8853c033e6e152
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-27 21:22:58 +0100

    Create Xerath.lua

---
+++ Xerath.lua
@@ -0,0 +1,50 @@
+Config = scriptConfig("Xerath", "Xerath:")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+--Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+
+spellData = 
+	{
+	[_Q] = {dmg = function () return 80 + 40*GetCastLevel(myHero,_Q) + 0.75*GetBonusAP(myHero) end, },
+	[_W] = {dmg = function () return 60 + 30*GetCastLevel(myHero,_W) + 0.6*GetBonusAP(myHero) end, },
+	[_E] = {dmg = function () return 80 + 30*GetCastLevel(myHero,_E) + 0.45*GetBonusAP(myHero) end, },
+	[_R] = {dmg = function () return 570 + 165*GetCastLevel(myHero,_R) + 1.29*GetBonusAP(myHero) end,
+	      range = function () return 1850 + 1050*GetCastLevel(myHero,_R) end },
+	}
+	
+
+OnLoop(function(myHero)
+        DamageCalc()
+        if GetKeyValue("Combo") then
+		local target = GetTarget(1600, DAMAGE_MAGIC)
+		        if ValidTarget(target, 1600) then
+				     
+					local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1400,200,1150,60,true,true)
+                    if GetButtonValue("E") then
+                     if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 then
+                     CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+                     end
+                    end
+					
+					local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,700,1000,200,true,true)
+                    if GetButtonValue("W") then
+                     if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 then
+                     CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
+                     end
+                    end
+					
+					local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),math.huge,600,1600,100,false,true)
+                    if GetButtonValue("Q") then
+                     if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
+                     CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+                     end
+                    end
+					if GetButtonValue("Q") then
+					if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
+                    CastSkillShot2(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+					end
+					end
+				end
+		end
+end)
