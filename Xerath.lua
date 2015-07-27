b6b510cda20351e4f21c95b9d81dc7958490b7df
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-27 21:27:54 +0100

    Update Xerath.lua

--- Xerath.lua
+++ Xerath.lua
@@ -15,7 +15,6 @@ spellData =
 	
 
 OnLoop(function(myHero)
-        DamageCalc()
         if GetKeyValue("Combo") then
 		local target = GetTarget(1600, DAMAGE_MAGIC)
 		        if ValidTarget(target, 1600) then
