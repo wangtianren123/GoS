dd0bced2a4c471cc873bd83a45e47c9e8a68bd8f
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-27 23:24:50 +0100

    Update Ahri.lua

--- Ahri.lua
+++ Ahri.lua
@@ -6,10 +6,10 @@ Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
 
 spellData = 
 	{
-	[_Q] = {dmg = function () return 80 + 50*GetCastLevel(myHero,_Q) + 0.70*GetBonusAP(myHero) end, },
-	[_W] = {dmg = function () return 64 + 40*GetCastLevel(myHero,_W) + 0.64*GetBonusAP(myHero) end, },
-	[_E] = {dmg = function () return 60 + 35*GetCastLevel(myHero,_E) + 0.5*GetBonusAP(myHero) end, },
-	[_R] = {dmg = function () return 210 + 120*GetCastLevel(myHero,_R) + 0.9*GetBonusAP(myHero) end },
+	[_Q] = {dmg = function () return 35 + 25*GetCastLevel(myHero,_Q) + 0.70*GetBonusAP(myHero) end, },
+	[_W] = {dmg = function () return 24 + 40*GetCastLevel(myHero,_W) + 0.64*GetBonusAP(myHero) end, },
+	[_E] = {dmg = function () return 25 + 35*GetCastLevel(myHero,_E) + 0.5*GetBonusAP(myHero) end, },
+	[_R] = {dmg = function () return 90 + 120*GetCastLevel(myHero,_R) + 0.9*GetBonusAP(myHero) end },
 	}
 
 OnLoop(function(myHero)
