a923b8b96fcda855e5875cede116f7fd39bc1f6e
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-27 15:25:24 +0100

    Update Blitzcrank.lua

--- Blitzcrank.lua
+++ Blitzcrank.lua
@@ -1,38 +1,35 @@
-AddInfo("Blitzcrank", "Blitzcrank")
-AddButton("Q", "Use Q", true)
-AddButton("W", "Use W", true)
-AddButton("E", "Use E", true)
-AddButton("R", "Use R", true)
+Config = scriptConfig("Blitzcrank", "Blitzcrank:")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
 
 OnLoop(function(myHero)
-
-	if GetKeyValue("Combo") then
-	local target = GetTarget(1000, DAMAGE_MAGIC)
-	    if ValidTarget(target, 1000) then
-		  local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,250,925,70,true,true)
-		        if GetButtonValue("Q") then
-             if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
-             CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-	           end
-            end
+	
+       if IWalkConfig.Combo then
+	      local target = GetTarget(1000, DAMAGE_MAGIC)
+	               if ValidTarget(target, 1000) then
+	              	
+	              	
+		              local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,250,925,70,true,true)
+                              if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
+                              CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+	                      end
+                          
 			
-		        if GetButtonValue("W") then
-                 if CanUseSpell(myHero, _W) == READY and not IsInDistance(target, 150) and IsInDistance(target, 400) then
-                 CastTargetSpell(myHero, _W)
-		         end
-		        end
+                              if CanUseSpell(myHero, _W) == READY and not IsInDistance(target, 150) and IsInDistance(target, 400) Config.W then
+                              CastTargetSpell(myHero, _W)
+		              end
 			
-		        if GetButtonValue("E") then
-                 if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 250) then
-                 CastTargetSpell(myHero, _E)
-		         end
-                end
-		  local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,250,0,550,false,true)	
-		          if GetButtonValue("R") then
-	             if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 then
-               CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
-	             end
-              end
-	    end
+                              if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 250) and Config.E then
+                              CastTargetSpell(myHero, _E)
+		              end
+		              
+		              local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,250,0,550,false,true)
+		              if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and Config.R then
+                              CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
+	                      end
+	                      
+                        end
 	end	
 end)
