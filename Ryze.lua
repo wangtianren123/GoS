a69e672bd249f37399edd1316a5467a239381b88
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-27 15:25:32 +0100

    Update Ryze.lua

--- Ryze.lua
+++ Ryze.lua
@@ -1,42 +1,35 @@
 local ARCANE_MASTERY = "ryzepassivestack"
 local SUPERCHARGED = "ryzepassivecharged"
 
-AddInfo("Ryze", "Ryze")
-AddButton("Q", "Use Q", true)
-AddButton("W", "Use W", true)
-AddButton("E", "Use E", true)
-AddButton("R", "Use R", true)
+Config = scriptConfig("Ryze", "Ryze:")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
 
 OnLoop(function(myHero)
 
-	if GetKeyValue("Combo") then
-	local target = GetTarget(1000, DAMAGE_MAGIC)
-	    if ValidTarget(target, 1000) then
-		local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,true,true)
+	if IWalkConfig.Combo then
+	       local target = GetTarget(1000, DAMAGE_MAGIC)
+	                if ValidTarget(target, 1000) then
 		
-		if GetButtonValue("Q") then
-                 if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
-                 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-	         end
-                end
+		
+		                local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1400,250,900,55,true,true)
+                                if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
+                                CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+	                        end
 			
-		if GetButtonValue("R") then
-                 if CanUseSpell(myHero, _R) == READY and (GotBuff(myHero, SUPERCHARGED) > 0 or GotBuff(myHero, ARCANE_MASTERY) > 3) then
-                 CastTargetSpell(myHero, _R)
-		 end
-		end
+                                if CanUseSpell(myHero, _R) == READY and (GotBuff(myHero, SUPERCHARGED) > 0 or GotBuff(myHero, ARCANE_MASTERY) > 3) and Config.R then
+                                CastTargetSpell(myHero, _R)
+		                end
 			
-		if GetButtonValue("W") then
-                 if CanUseSpell(myHero, _W) == READY then
-                 CastTargetSpell(target, _W)
-		 end
-                end
+                                if CanUseSpell(myHero, _W) == READY and Config.W then
+                                CastTargetSpell(target, _W)
+		                end
 			
-		if GetButtonValue("E") then
-	         if CanUseSpell(myHero, _E) == READY then
-                 CastTargetSpell(target, _E)
-		 end
-                end
-	    end
+	                        if CanUseSpell(myHero, _E) == READY and Config.E then
+                                CastTargetSpell(target, _E)
+		                end
+	                end
 	end	
 end)
