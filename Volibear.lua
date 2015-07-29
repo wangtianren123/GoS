8feaa331348457329e5830b14dd4de2ecdb33664
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-29 19:10:23 +0100

    Update Volibear.lua

--- Volibear.lua
+++ Volibear.lua
@@ -2,10 +2,11 @@ Config = scriptConfig("Volibear", "Volibear:")
 Config.addParam("W", "Use W if killable", SCRIPT_PARAM_ONOFF, true)
 PrintChat("VolibearAutoW by Deftsu. Enjoy :)")
 OnLoop(function(myHero)
-
-local Wdamage = CalcDamage(myHero, target, ((GetCastLevel(myHero,_W)-1)*45+80+(GetMaxHP(myHero)-(440+GetLevel(myHero)*86))*.15)*(1+(GetMaxHP(target)-GetCurrentHP(target))/GetMaxHP(target)), 0)
+	
+	
         if IWalkConfig.Combo then
 		local target = GetTarget(400, DAMAGE_PHYSICAL)
+		local Wdamage = CalcDamage(myHero, target, ((GetCastLevel(myHero,_W)-1)*45+80+(GetMaxHP(myHero)-(440+GetLevel(myHero)*86))*.15)*(1+(GetMaxHP(target)-GetCurrentHP(target))/GetMaxHP(target)), 0)
 		        if ValidTarget(target, 400) then
 				   if CanUseSpell(myHero, _W) == READY and Config.W and Wdamage > GetCurrentHP(target) then
 				   CastTargetSpell(target, _W)
