8a20c322631849ae211fe74eb7be9499009354ba
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-30 04:03:32 +0100

    Update Baseult.lua

--- Baseult.lua
+++ Baseult.lua
@@ -1,45 +1,51 @@
-function enemyBasePos()
- if GetTeam(myHero) == 100 then enemyBasePos = Vector(14340, 171, 14390)
- elseif GetTeam(myHero) == 200 then enemyBasePos = Vector(529, -36, 4169)
- end
+
+local enemyBasePos, delay, missileSpeed, damage, recallPos = nil, 0, 0, nil, nil
+BaseUltConfig = scriptConfig("BaseUlt", "BaseUlt")
+BaseUltConfig.addParam("doIt", "Do BaseUlt", SCRIPT_PARAM_ONOFF, true) 
+myHero = GetMyHero()
+
+if GetTeam(myHero) == 100 then 
+	enemyBasePos = Vector(14340, 171, 14390)
+elseif GetTeam(myHero) == 200 then 
+	enemyBasePos = Vector(529, -36, 4169)
 end
 
+if GetObjectName(myHero) == "Ashe" then
+	delay = 250
+	missileSpeed = 1600
+	damage = function(target) return CalcDamage(myHero, target, 0, 75 + 175*GetCastLevel(myHero,_R) + GetBonusAP(myHero)) end
+elseif GetObjectName(myHero) == "Draven" then
+	delay = 400
+	missileSpeed = 2000
+	damage = function(target) return CalcDamage(myHero, target, 75 + 100*GetCastLevel(myHero,_R) + 1.1*GetBonusDmg(myHero)) end
+elseif GetObjectName(myHero) == "Ezreal" then
+	delay = 1000
+	missileSpeed = 2000
+	damage = function(target) return CalcDamage(myHero, target, 0, 200 + 150*GetCastLevel(myHero,_R) + .9*GetBonusAP(myHero)+GetBonusDmg(myHero)) end
+elseif GetObjectName(myHero) == "Jinx" then
+	delay = 600
+	missileSpeed = 2300
+	damage = function(target) return CalcDamage(myHero, target, (GetMaxHP(target)-GetCurrentHP(target))*(0.2+0.05*GetCastLevel(myHero, _R)) + 150 + 100*GetCastLevel(myHero,_R) + GetBonusDmg(myHero)) end
+end
 
-OnLoop(function(myHero)
- local target = GetCurrentTarget()
- if ValidTarget(target, 20000) then
- local myHeroPos = GetOrigin(myHero)
-  if GetObjectName(myHero) == "Ashe" then
-    delay = 250
-    missileSpeed = 1600
-    local damage = CalcDamage(myHero, target, 0, 75 + 175*GetCastLevel(myHero,_R) + GetBonusAP(myHero))
-  elseif GetObjectName(myHero) == "Draven" then
-    delay = 400
-    missileSpeed = 2000
-    local damage = CalcDamage(myHero, target, 75 + 100*GetCastLevel(myHero,_R) + 1.1*GetBonusDmg(myHero), 0)
-  elseif GetObjectName(myHero) == "Ezreal" then
-    delay = 1000
-    missileSpeed = 2000
-    local damage = CalcDamage(myHero, target, 0, 200 + 150*GetCastLevel(myHero,_R) + .9*GetBonusAP(myHero)+GetBonusDmg(myHero))
-  elseif GetObjectName(myHero) == "Jinx" then
-    delay = 600
-    missileSpeed = 1700
-    local damage = CalcDamage(myHero, target, 150 + 100*GetCastLevel(myHero,_R) + 0.5*GetBonusDmg(myHero), 0 )
-  end
-
- end
-end)
-
-if CanUseSpell(_R) == READY then
-   if damage > GetCurrentHP(target) then
-        local timeToRecall = recallProc.totalTime
-        local timeToHit = delay + (distance / missileSpeed)
-		local distance = GetDistance(enemyBasePos)
-        if timeToRecall > TimeToHit then
-        DelayAction(function() CastSkillShot(_R, enemyBasePos.x, enemyBasePos.y, enemyBasePos.z) end, (timeToRecall-timeToHit) * 1000)
-        end
-    end
- end
-
- OnProcessRecall(function(Object,recallProc)
+OnProcessRecall(function(Object,recallProc)
+	if CanUseSpell(myHero, _R) == READY and BaseUltConfig.doIt and GetTeam(Object) ~= GetTeam(myHero) then
+		if damage(Object) > GetCurrentHP(Object) then
+			local timeToRecall = recallProc.totalTime
+			local distance = GetDistance(enemyBasePos)
+			local timeToHit = delay + (distance * 1000 / missileSpeed)
+			if timeToRecall > timeToHit then
+				recallPos = Vector(Object)
+				DelayAction(
+					function() 
+						if recallPos == Vector(Object) then
+							CastSkillShot(_R, enemyBasePos.x, enemyBasePos.y, enemyBasePos.z)
+							recallPos = nil
+						end
+					end, 
+					timeToRecall-timeToHit
+				)
+			end
+		end
+	end
 end)
