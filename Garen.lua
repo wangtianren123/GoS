070f0d194cbc0c0c6f122b06a8949a5e853ac4ef
D3ftsu <ahmedtayachon1@gmail.com>
2015-07-31 03:09:00 +0100

    Create Garen.lua

---
+++ Garen.lua
@@ -0,0 +1,24 @@
+Config = scriptConfig("Garen", "Garen:")
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R if Killable", SCRIPT_PARAM_ONOFF, true)
+PrintChat("D3ftland Garen By Deftsu.")
+
+OnLoop(function(myHero)
+        if IWalkConfig.Combo then
+		    local target = GetTarget(400, DAMAGE_PHYSICAL)
+		        if ValidTarget(target, 400) then
+				
+				    if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 300) and Config.E then
+				    CastSpell(_E)
+					end
+					
+			    
+			     
+				    local target = GetTarget(400, DAMAGE_MAGIC)
+					if CanUseSpell(myHero, _R) == READY and IsInDistance(target, 400) and Config.R and CalcDamage(myHero, target, 0, 175*GetCastLevel(myHero,_R)+(GetMaxHP(target)-GetCurrentHP(target))/((8-GetCastLevel(myHero,_R))/2) > GetCurrentHP(target) then
+					CastTargetSpell(target, _E)
+					end
+				end
+		    end
+		end
+end)
