e8e32d6497982c3c530ac85b0df0a1a1019a57ad
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-03 00:56:00 +0100

    Update Ahri.lua

--- Ahri.lua
+++ Ahri.lua
@@ -1,29 +1,34 @@
-Config = scriptConfig("Ahri", "Ahri:")
+Config = scriptConfig("Ahri", "Ahri")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("KSQ", "KillSteal with Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("KSW", "KillSteal with W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("KSE", "KillSteal with E", SCRIPT_PARAM_ONOFF, false)
-Config.addParam("HarassQ", "Harass Q (C)", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("HarassW", "Harass W (C)", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("HarassE", "Harass E (C)", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, false)
-Config.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, false)
-Config.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, false)
+KSConfig = scriptConfig("KS", "Killsteal:")
+KSConfig.addParam("KSQ", "Killsteal with Q", SCRIPT_PARAM_ONOFF, true)
+KSConfig.addParam("KSW", "Killsteal with W", SCRIPT_PARAM_ONOFF, true)
+KSConfig.addParam("KSE", "Killsteal with E", SCRIPT_PARAM_ONOFF, false)
+HarassConfig = scriptConfig("Harass", "Harass:")
+HarassConfig.addParam("HarassQ", "Harass Q (C)", SCRIPT_PARAM_ONOFF, true)
+HarassConfig.addParam("HarassW", "Harass W (C)", SCRIPT_PARAM_ONOFF, true)
+HarassConfig.addParam("HarassE", "Harass E (C)", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig = scriptConfig("Drawings", "Drawings:")
+DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, false)
+DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, false)
+DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, false)
+
+myIAC = IAC()
 
 OnLoop(function(myHero)
 Drawings()
 Killsteal()
 
-    if IWalkConfig.Combo then
+        if IWalkConfig.Combo then
         
-        local target = GetTarget(1000, DAMAGE_MAGIC)
+                local target = GetTarget(1000, DAMAGE_MAGIC)
 		if ValidTarget(target, 1000) then
 				
-			        local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1550,250,GetCastRange(myHero,_E),60,true,true)
+			        local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1500,250,GetCastRange(myHero,_E),60,true,true)
                                 if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and Config.E then
                                 CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
                                 end
@@ -31,7 +36,7 @@ Killsteal()
 				local mousePos = GetMousePos()
 				local ComboDmg = CalcDamage(myHero, target, 0, (35 + 25*GetCastLevel(myHero,_Q) + 0.70*GetBonusAP(myHero)+ 24 + 40*GetCastLevel(myHero,_W) + 0.64*GetBonusAP(myHero) + 25 + 35*GetCastLevel(myHero,_E) + 0.5*GetBonusAP(myHero)))
 			        if CanUseSpell(myHero, _R) == READY and ComboDmg < GetCurrentHP(target) and Config.R then
-			        CastSkillShot(_R,mousePos.x,mousePos.y,mousePos.z)	
+			        CastSkillShot(_R,mousePos.x,mousePos.y,mousePos.z)
 				end
 					
 			        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),2500,250,GetCastRange(myHero,_Q),100,false,true)
@@ -45,21 +50,24 @@ Killsteal()
 	        end
 	end
 end)
+
 OnLoop(function(myHero)
 
 	if IWalkConfig.Harass then
-	    local target = GetTarget(1000, DAMAGE_MAGIC)
+	        local target = GetTarget(1000, DAMAGE_MAGIC)
 		local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),2500,250,880,100,false,true)
 		local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1550,250,975,60,true,true)
 		if ValidTarget(target, 1000) then
-		            
-                    if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target,GetCastRange(myHero,_E)) and Config.HarassE then
-                    CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
-                    elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target,GetCastRange(myHero,_Q)) and Config.HarassQ then 
-                    CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-                    elseif CanUseSpell(myHero, _W) == READY and ValidTarget(target,GetCastRange(myHero,_W)) and CanUseSpell(myHero, _W) == READY and Config.HarassW then
-                    CastSpell(_W)
-                    end
+		
+		    if (GetCurrentMana(myHero)/GetMaxMana(myHero)) > 0.4 then
+                       if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target,GetCastRange(myHero,_E)) and HarassConfig.HarassE then
+                       CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+                       elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target,GetCastRange(myHero,_Q)) and HarassConfig.HarassQ then 
+                       CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+                       elseif CanUseSpell(myHero, _W) == READY and ValidTarget(target,GetCastRange(myHero,_W)) and HarassConfig.HarassW then
+                       CastSpell(_W)
+                       end
+		    end
 		end
 	end
 end)
@@ -67,12 +75,12 @@ end)
 function Killsteal()
 	for i,enemy in pairs(GetEnemyHeroes()) do
 	    local QPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),2500,250,GetCastRange(myHero,_Q),100,false,true)
-		if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_Q)) and Config.KSQ and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (35 + 25*GetCastLevel(myHero,_Q) + 0.70*GetBonusAP(myHero))) then 
+		if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_Q)) and KSConfig.KSQ and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (35 + 25*GetCastLevel(myHero,_Q) + 0.70*GetBonusAP(myHero))) then 
 		CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-		elseif CanUseSpell(myHero, _W) and ValidTarget(enemy,GetCastRange(myHero,_W)) and Config.KSW and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (24 + 40*GetCastLevel(myHero,_W) + 0.64*GetBonusAP(myHero))) then
+		elseif CanUseSpell(myHero, _W) and ValidTarget(enemy,GetCastRange(myHero,_W)) and KSConfig.KSW and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (24 + 40*GetCastLevel(myHero,_W) + 0.64*GetBonusAP(myHero))) then
 		CastSpell(_W)
 		local EPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),1550,250,GetCastRange(myHero,_E),60,true,true)
-		elseif CanUseSpell(myHero, _E) and ValidTarget(enemy,GetCastRange(myHero,_E)) and Config.KSE and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (24 + 40*GetCastLevel(myHero,_W) + 0.64*GetBonusAP(myHero))) then
+		elseif CanUseSpell(myHero, _E) and ValidTarget(enemy,GetCastRange(myHero,_E)) and KSConfig.KSE and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (24 + 40*GetCastLevel(myHero,_W) + 0.64*GetBonusAP(myHero))) then
 		CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 		end
 	end
@@ -80,9 +88,9 @@ end
 
 function Drawings()
 myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and Config.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _W) == READY and Config.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _E) == READY and Config.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _R) == READY and Config.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
 end
 AddGapcloseEvent(_E, 975, false)
