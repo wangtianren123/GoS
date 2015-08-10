cd0467e4e4c2cdef593cd9ca55fa53604c860841
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-10 21:29:48 +0100

    Update Vayne.lua

--- Vayne.lua
+++ Vayne.lua
@@ -1,7 +1,49 @@
-Config = scriptConfig("Vayne", "Vayne.lua")
-Config.addParam("CA", "Condemn Auto", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("CC", "Condemn Combo", SCRIPT_PARAM_ONOFF, true)
 require 'MapPositionGOS'
+PrintChat("D3ftland Vayne By Deftsu Loaded, Have A Good Game!")
+Config = scriptConfig("Vayne", "Vayne")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("AutoE", "Auto E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("Walltumble1", "Walltumble Mid", SCRIPT_PARAM_KEYDOWN, string.byte("T"))
+Config.addParam("Walltumble2", "Walltumble Drake", SCRIPT_PARAM_KEYDOWN, string.byte("U"))
+--Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig = scriptConfig("Drawings", "Drawings:")
+DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
+
+myIAC = IAC()
+
+OnLoop(function(myHero)
+Drawings()
+Walltumble()
+if Config.AutoE then
+AutoE()
+end
+
+if IWalkConfig.Combo then
+        
+                local target = GetTarget(550, DAMAGE_PHYSICAL)
+		if ValidTarget(target, 550) then
+Tumble()
+end
+end)
+
+function Tumble()
+local HeroPos = GetOrigin(myHero)
+local mousePos = GetMousePos()
+local AfterTumblePos = HeroPos + (Vector(mousePos) - HeroPos):normalized() * 300
+local DistanceAfterTumble = GetDistance(AfterTumblePos, Target)
+if  DistanceAfterTumble < 630*630 and DistanceAfterTumble > 300*300 then
+CastSkillShot(_Q, mousePos.x, mousePos.y, mousePos.z)
+end
+if GetDistance(Target) > 630*630 and DistanceAfterTumble < 630*630 then
+CastSkillShot(_Q, mousePos.x, mousePos.y, mousePos.z)
+end 
+
+end
+
+OnProcessSpell(function(unit, spell)
+if unit and unit == myHero and spell and spell.name and spell.name:lower():find("attack") then
+DelayAction(function() Tumble() end, spell.windUpTime + GetLatency()/2000
+end)
 
 function AutoE()
 	 for _,target in pairs(GetEnemyHeroes()) do
@@ -22,9 +64,9 @@ function AutoE()
 			local self=GetOrigin(myHero)
 			selfx = self.x
 			selfy = self.y
-    	selfz = self.z
+    	                selfz = self.z
 			local HeroPos = Vector(selfx, selfy, selfz)
-    	------------------------------
+    	
 			local Pos1 = TargetPos-(TargetPos-HeroPos)*(-distance1/GetDistance(target))
 			local Pos2 = TargetPos-(TargetPos-HeroPos)*(-distance2/GetDistance(target))
 			local Pos3 = TargetPos-(TargetPos-HeroPos)*(-distance3/GetDistance(target))
@@ -64,3 +106,28 @@ function AutoE()
 		end
 	end
 end
+
+function WallTumble()
+local HeroPos = GetOrigin(myHero)
+
+    if CanUseSpell(myHero, _Q) == READY then
+        if Config.Walltumble1 and HeroPos.x == 6962 and HeroPos.z == 8952 then
+            CastSkillShot(_Q,6667.3271484375, 0, 8794.64453125)
+        else
+            MoveToXYZ(6962, 0, 8952)
+        end
+    
+        if Config.Walltumble2 and HeroPos.x == 12060 and HeroPos.z == 4806 then
+            CastSkillShot(_Q,11745.198242188, 0, 4625.4379882813)
+        else
+            MoveToXYZ(12060, 0, 4806)
+        end
+    end
+end
+
+            
+function Drawings()
+myHeroPos = GetOrigin(myHero)
+if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
+end
+AddGapcloseEvent(_E, 475, true)
