457b0ec8dc18ef3e8f33ca47812c97724332739d
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-12 14:19:10 +0100

    Create Viktor.lua

---
+++ Viktor.lua
@@ -0,0 +1,52 @@
+Config = scriptConfig("Viktor", "Viktor")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+KSConfig = scriptConfig("KS", "Killsteal:")
+KSConfig.addParam("KSQ", "Killsteal with Q", SCRIPT_PARAM_ONOFF, true)
+KSConfig.addParam("KSE", "Killsteal with E", SCRIPT_PARAM_ONOFF, true)
+KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, true)
+HarassConfig = scriptConfig("Harass", "Harass:")
+HarassConfig.addParam("HarassQ", "Harass Q (C)", SCRIPT_PARAM_ONOFF, true)
+HarassConfig.addParam("HarassE", "Harass E (C)", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig = scriptConfig("Drawings", "Drawings:")
+DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
+
+myIAC = IAC()
+
+
+OnLoop(function(myHero)
+Drawings()
+--Killsteal()
+        local target = GetTarget(1200, DAMAGE_MAGIC)
+		if ValidTarget(target, 1200) then
+            
+	           
+				if IWalkConfig.Combo then	
+					local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1200,0,1000,90,false,true)
+					local myHeroPos = GetMyHeroPos()
+                    local distance = GetDistance(myHero, target)
+                    local castPos = (1200*EPred.Pos+(distance - 1200)*myHeroPos)/distance
+					if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and Config.E then
+					CastSkillShot3(_E,castPos,EPred.PredPos)
+					end
+				end
+		end
+end)
+
+--function Killsteal()
+	--for i,enemy in pairs(GetEnemyHeroes()) do
+	--end
+--end
+
+function Drawings()
+myHeroPos = GetOrigin(myHero)
+if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
+end
