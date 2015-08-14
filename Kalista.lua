76d4f4acaf24c0a1487b1783ffab5486c54b62f2
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-14 20:54:45 +0100

    Update Kalista.lua

--- Kalista.lua
+++ Kalista.lua
@@ -1,12 +1,9 @@
-PrintChat("D3ftland Kalista By Deftsu Loaded, Have A Good Game!")
-Config = scriptConfig("Kalista", "Kalista")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("KS", "Use Smart KS", SCRIPT_PARAM_ONOFF, true)
-HarassConfig = scriptConfig("Harass", "Harass:")
-HarassConfig.addParam("HarassQ", "Harass Q (C)", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawHP", "Draw Damage", SCRIPT_PARAM_ONOFF, true)
+OnProcessSpell(function(unit, spell)
+ for _, ally in pairs(GetAllyHeroes()) do
+  if unit and unit == myHero and spell then
+    if spell.name:lower():find("kalistapspellcast") then
+      PrintChat("Nebel is Pledged to you")
+    end
+  end
+ end
+end)
