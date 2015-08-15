43dfc591dc9c8584d07b6cf52afa5e46d2db78f8
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-15 21:05:38 +0100

    Update Kalista.lua

--- Kalista.lua
+++ Kalista.lua
@@ -1,7 +1,7 @@
 PrintChat("D3ftland Kalista By Deftsu Loaded, Have A Good Game!")
 Config = scriptConfig("Kalista", "Kalista")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("Autolvl", "Autolvl E-Q-W", SCRIPT_PARAM_ONOFF, false)
 Config.addParam("Item1", "Use BotRK", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("Item2", "Use Bilgewater", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("Item3", "Use Youmuu", SCRIPT_PARAM_ONOFF, true)
@@ -16,19 +16,77 @@ DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawEdmg", "Draw E Dmg %", SCRIPT_PARAM_ONOFF, true)
+JSConfig = scriptConfig("JS", "Junglesteal:")
+JSConfig.addParam("baron","Baron", SCRIPT_PARAM_ONOFF, true)
+JSConfig.addParam("dragon","Dragon", SCRIPT_PARAM_ONOFF, true)
+JSConfig.addParam("red","Red", SCRIPT_PARAM_ONOFF, true)
+JSConfig.addParam("blue","Blue", SCRIPT_PARAM_ONOFF, true)
+
+OnLoop(function(myHero)
+Drawings()
+
+if Config.Autolvl then
+LevelUp()
+end
+end)
 
 OnProcessSpell(function(unit, spell)
  for _, ally in pairs(GetAllyHeroes()) do
   if unit and unit == myHero and spell then
-    if spell.name:lower():find("kalistapinvocation") then
-      PrintChat("Nebel is Pledged to you "..GetObjectName(spell.target).."")
+    if spell.name:lower():find("kalistapspellcast") then
+      PrintChat("You are now pledged to "..GetObjectName(spell.target).."")
     end
   end
  end
 end)
 
---function Killsteal()
+function Killsteal()
+   for i,enemy in pairs(GetEnemyHeroes()) do
+       if CanUseSpell(myHero, _E) == READY and ValidTarget(enemy,GetCastRange(myHero,_E)) and KSConfig.KSE and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, (GotBuff(enemy,"kalistaexpungemarker") > 0 and (10 + (10 * GetCastLevel(myHero,_E)) + (GetBonusDmg(myHero)+GetBaseDamage(myHero) * 0.6)) + (GotBuff(enemy,"kalistaexpungemarker")-1) * (kalE(GetCastLevel(myHero,_E)) + (0.175 + 0.025 * GetCastLevel(myHero,_E))*GetBonusDmg(myHero)+GetBaseDamage(myHero)) or 0), 0) then
+	   CastSpell(_E)
+	   elseif CanUseSpell(myHero, _Q) == READY and ValidTarget(enemy, GetCastRange(myHero, _Q)) and KSConfig.KSQ and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, 0) then  
+        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 
+function LevelUp()     
+
+if GetLevel(myHero) == 1 then
+	LevelSpell(_E)
+elseif GetLevel(myHero) == 2 then
+	LevelSpell(_W)
+elseif GetLevel(myHero) == 3 then
+	LevelSpell(_Q)
+elseif GetLevel(myHero) == 4 then
+        LevelSpell(_E)
+elseif GetLevel(myHero) == 5 then
+        LevelSpell(_E)
+elseif GetLevel(myHero) == 6 then
+	LevelSpell(_R)
+elseif GetLevel(myHero) == 7 then
+	LevelSpell(_E)
+elseif GetLevel(myHero) == 8 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 9 then
+        LevelSpell(_E)
+elseif GetLevel(myHero) == 10 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 11 then
+        LevelSpell(_R)
+elseif GetLevel(myHero) == 12 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 13 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 14 then
+        LevelSpell(_W)
+elseif GetLevel(myHero) == 15 then
+        LevelSpell(_W)
+elseif GetLevel(myHero) == 16 then
+        LevelSpell(_R)
+elseif GetLevel(myHero) == 17 then
+        LevelSpell(_W)
+elseif GetLevel(myHero) == 18 then
+        LevelSpell(_W)
+end
+end
 
 function Drawings()
 local HeroPos = GetOrigin(myHero)
