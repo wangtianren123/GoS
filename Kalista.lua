702b4545fafcab347bac86a267beccf6f3d4bea1
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-18 19:45:48 +0100

    Update Kalista.lua

--- Kalista.lua
+++ Kalista.lua
@@ -31,7 +31,7 @@ myIAC = IAC()
 OnLoop(function(myHero)
 Drawings()
 Killsteal()
-
+Junglesteal()
 if ExtraConfig.Autolvl then
 LevelUp()
 end
@@ -157,9 +157,9 @@ end
 
 function SaveAlly()
  for _, ally in pairs(GetAllyHeroes()) do
+ for i,enemy in pairs(GetEnemyHeroes()) do
    local soulboundhero = GotBuff(ally, "kalistacoopstrikeally") > 0
-   local allyPos = GetOrigin(ally)
-   if soulboundhero and GetCurrentHP(ally)/GetMaxHP(ally) < 0.05 and EnemiesAround(allyPos, 1500) > 0 then
+   if soulboundhero and GetCurrentHP(ally)/GetMaxHP(ally) < 0.05 and GetDistance(ally, enemy) < 1500 then 
    CastSpell(_R)
    end
  end
@@ -186,3 +186,20 @@ function kalE(x)
 if x <= 1 then return 10 else return kalE(x-1) + 2 + x
 end 
 end -- too smart for you inspired, thanks for this anyway :3, lazycat
+
+function Junglesteal()
+  for _,mob in pairs(GetAllMinions(MINION_JUNGLE)) do
+  local Damage = CalcDamage(myHero, mob, GotBuff(mob,"kalistaexpungemarker") > 0 and (10 + (10 * GetCastLevel(myHero,_E)) + ((GetBonusDmg(myHero)+GetBaseDamage(myHero)) * 0.6)) + (GotBuff(mob,"kalistaexpungemarker")-1) * (kalE(GetCastLevel(myHero,_E)) + (0.175 + 0.025 * GetCastLevel(myHero,_E))*(GetBonusDmg(myHero)+GetBaseDamage(myHero))) or 0)
+    if CanUseSpell(myHero, _E) == READY and GetObjectName(mob) == "SRU_Baron12.1.1" and ValidTarget(mob,GetCastRange(myHero,_E)) and JSConfig.baron and GetCurrentHP(mob) < Damage then
+	CastSpell(_E)
+	elseif CanUseSpell(myHero, _E) == READY and GetObjectName(mob) == "SRU_Dragon6.1.1" and ValidTarget(mob,GetCastRange(myHero,_E)) and JSConfig.baron and GetCurrentHP(mob) < Damage then
+	CastSpell(_E)
+	end
+	
+	local mobPos = GetOrigin(mob)
+    local drawPos = WorldToScreen(1,mobPos.x,mobPos.y,mobPos.z)
+	if Damage > 0 and ValidTarget(mob,GetCastRange(myHero,_E)) then 
+    DrawText(math.floor(Damage/GetCurrentHP(mob)*100).."%",36,drawPos.x+40,drawPos.y+30,0xffffffff)
+    end
+  end
+end
