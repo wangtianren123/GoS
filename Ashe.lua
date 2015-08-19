6ddb8bd0ac9f9a8b94cb97886dfdefbc8bc93128
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-19 12:32:05 +0100

    Update Ashe.lua

--- Ashe.lua
+++ Ashe.lua
@@ -3,6 +3,13 @@ Config = scriptConfig("Ashe", "Ashe")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, false)
+MiscConfig = scriptConfig("Misc", "Misc")
+MiscConfig.addParam("Autolvl", "Autolvl Q-W-E", SCRIPT_PARAM_ONOFF, false)
+MiscConfig.addParam("Item1", "Use BotRK", SCRIPT_PARAM_ONOFF, true)
+MiscConfig.addParam("Item2", "Use Bilgewater", SCRIPT_PARAM_ONOFF, true)
+MiscConfig.addParam("Item3", "Use Youmuu", SCRIPT_PARAM_ONOFF, true)
+MiscConfig.addParam("Item4", "Use QSS", SCRIPT_PARAM_ONOFF, true)
+MiscConfig.addParam("Item5", "Use Mercurial", SCRIPT_PARAM_ONOFF, true)
 KSConfig = scriptConfig("KS", "Killsteal")
 KSConfig.addParam("KSW", "Killsteal with W", SCRIPT_PARAM_ONOFF, true)
 KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, true)
@@ -14,13 +21,58 @@ DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
 BaseUltConfig = scriptConfig("BaseUlt", "BaseUlt")
 BaseUltConfig.addParam("doIt", "BaseUlt (broken)", SCRIPT_PARAM_ONOFF, true) 
 
-myHero = GetMyHero()
-
 myIAC = IAC()
+
+CHANELLING_SPELLS = {
+    ["Caitlyn"]                     = {_R},
+    ["Katarina"]                    = {_R},
+    ["MasterYi"]                    = {_W},
+    ["FiddleSticks"]                = {_W, _R},
+    ["Galio"]                       = {_R},
+    ["Lucian"]                      = {_R},
+    ["MissFortune"]                 = {_R},
+    ["VelKoz"]                      = {_R},
+    ["Nunu"]                        = {_R},
+    ["Shen"]                        = {_R},
+    ["Karthus"]                     = {_R},
+    ["Malzahar"]                    = {_R},
+    ["Pantheon"]                    = {_R},
+    ["Warwick"]                     = {_R},
+    ["Xerath"]                      = {_R},
+}
+
+local callback = nil
+ 
+OnProcessSpell(function(unit, spell)    
+    if not callback or not unit or GetObjectType(unit) ~= Obj_AI_Hero  or GetTeam(unit) == GetTeam(GetMyHero()) then return end
+    local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
+ 
+        if unitChanellingSpells then
+            for _, spellSlot in pairs(unitChanellingSpells) do
+                if spell.name == GetCastName(unit, spellSlot) then callback(unit, CHANELLING_SPELLS) end
+            end
+		end
+end)
+ 
+function addInterrupterCallback( callback0 )
+        callback = callback0
+end
 
 OnLoop(function(myHero)
 Drawings()
 Killsteal()
+
+if MiscConfig.Autolvl then
+LevelUp()
+end
+
+if GetItemSlot(myHero,3140) > 0 and MiscConfig.Item4 and GotBuff(myHero, "Stun") > 0 or GotBuff(myHero, "suppression") > 0 or GotBuff(myHero, "mordekaiserchildrenofthegrave") > 0 or GotBuff(myHero, "bruammark") > 0 or GotBuff(myHero, "zedulttargetmark") > 0 or GotBuff(myHero, "fizzmarinerdoombomb") > 0 or GotBuff(myHero, "soulshackles") > 0 or GotBuff(myHero, "varusrsecondary") > 0 or GotBuff(myHero, "vladimirhemoplague") > 0 or GotBuff(myHero, "urgotswap2") > 0 or GotBuff(myHero, "skarnerimpale") > 0 or GotBuff(myHero, "poppydiplomaticimmunity") > 0 or GotBuff(myHero, "leblancsoulshackle") > 0 or GotBuff(myHero, "leblancsoulshacklem") > 0 and GetCurrentHP(myHero)/GetMaxHP(myHero) < 0.75 then
+CastTargetSpell(myHero, GetItemSlot(myHero,3140))
+end
+
+if GetItemSlot(myHero,3139) > 0 and MiscConfig.Item5 and GotBuff(myHero, "Stun") > 0 or GotBuff(myHero, "suppression") > 0 or GotBuff(myHero, "mordekaiserchildrenofthegrave") > 0 or GotBuff(myHero, "bruammark") > 0 or GotBuff(myHero, "zedulttargetmark") > 0 or GotBuff(myHero, "fizzmarinerdoombomb") > 0 or GotBuff(myHero, "soulshackles") > 0 or GotBuff(myHero, "varusrsecondary") > 0 or GotBuff(myHero, "vladimirhemoplague") > 0 or GotBuff(myHero, "urgotswap2") > 0 or GotBuff(myHero, "skarnerimpale") > 0 or GotBuff(myHero, "poppydiplomaticimmunity") > 0 or GotBuff(myHero, "leblancsoulshackle") > 0 or GotBuff(myHero, "leblancsoulshacklem") > 0 and GetCurrentHP(myHero)/GetMaxHP(myHero) < 0.75 then
+CastTargetSpell(myHero, GetItemSlot(myHero,3139))
+end	
 
         local target = GetTarget(1000, DAMAGE_PHYSICAL)
 	if ValidTarget(target, 1000) then
@@ -40,6 +92,18 @@ Killsteal()
                         if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and GetCurrentHP(target)/GetMaxHP(target) < 0.5 and Config.R then
                         CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 	                end
+	                
+	                if GetItemSlot(myHero,3153) > 0 and MiscConfig.Item1 and GetCurrentHP(myHero)/GetMaxHP(myHero) < 0.5 and GetCurrentHP(target)/GetMaxHP(target) > 0.2 then
+                        CastTargetSpell(target, GetItemSlot(myHero,3153))
+                        end
+
+                        if GetItemSlot(myHero,3144) > 0 and MiscConfig.Item2 and GetCurrentHP(myHero)/GetMaxHP(myHero) < 0.5 and GetCurrentHP(target)/GetMaxHP(target) > 0.2 then
+                        CastTargetSpell(target, GetItemSlot(myHero,3144))
+                        end
+
+                        if GetItemSlot(myHero,3142) > 0 and MiscConfig.Item3 then
+                        CastTargetSpell(GetItemSlot(myHero,3142))
+                        end
 		end
 	end
 		
@@ -73,8 +137,57 @@ function Killsteal()
 	end
 end
 
+function LevelUp()     
+
+if GetLevel(myHero) == 1 then
+	LevelSpell(_W)
+elseif GetLevel(myHero) == 2 then
+	LevelSpell(_Q)
+elseif GetLevel(myHero) == 3 then
+	LevelSpell(_E)
+elseif GetLevel(myHero) == 4 then
+        LevelSpell(_W)
+elseif GetLevel(myHero) == 5 then
+        LevelSpell(_W)
+elseif GetLevel(myHero) == 6 then
+	LevelSpell(_R)
+elseif GetLevel(myHero) == 7 then
+	LevelSpell(_W)
+elseif GetLevel(myHero) == 8 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 9 then
+        LevelSpell(_W)
+elseif GetLevel(myHero) == 10 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 11 then
+        LevelSpell(_R)
+elseif GetLevel(myHero) == 12 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 13 then
+        LevelSpell(_Q)
+elseif GetLevel(myHero) == 14 then
+        LevelSpell(_E)
+elseif GetLevel(myHero) == 15 then
+        LevelSpell(_E)
+elseif GetLevel(myHero) == 16 then
+        LevelSpell(_R)
+elseif GetLevel(myHero) == 17 then
+        LevelSpell(_E)
+elseif GetLevel(myHero) == 18 then
+        LevelSpell(_E)
+end
+end
+
 function Drawings()
 myHeroPos = GetOrigin(myHero)
 if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
 end
+
+addInterrupterCallback(function(target, spellType)
+local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1600,250,2000,130,false,true)
+  if IsInDistance(target, 1000) and CanUseSpell(myHero,_R) == READY and spellType == CHANELLING_SPELLS then
+    CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
+  end
+end)
+
 AddGapcloseEvent(_R, 1000, false)
