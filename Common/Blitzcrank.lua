5748691ce33b81ba91a156f2869e5cada5bd3539
D3ftsu <ahmedtayachon1@gmail.com>
2015-08-29 15:31:24 +0100

    Update Blitzcrank.lua

--- Common/Blitzcrank.lua
+++ Common/Blitzcrank.lua
@@ -21,49 +21,36 @@ Killsteal()
 
         if IWalkConfig.Combo then
 	      local target = GetTarget(1000, DAMAGE_MAGIC)
-	        if ValidTarget(target, 1000) then
-	              	
 	              	
 		        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,250,GetCastRange(myHero,_Q),70,true,true)
                         if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	                end
                           
-                        if CanUseSpell(myHero, _W) == READY and not IsInDistance(target, 150) and IsInDistance(target, 700) and Config.W then
+                        if CanUseSpell(myHero, _W) == READY and not IsInDistance(target, 150) and Config.W then
                         CastSpell(_W)
 		        end
 			
-                        if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 250) and IsInDistance(target, 250) and Config.E then
+                        if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 250) and Config.E then
                         CastSpell(_E)
 		        end
 		              
-		        if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) and IsInDistance(target, 550) and Config.R then
+		        if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
                         CastSpell(_R)
 	                end
 	                      
-                end
 	end	
 	
 	if IWalkConfig.Harass then
 	     local target = GetTarget(1000, DAMAGE_MAGIC)
-		              
-		if ValidTarget(target, 1000) then
 		
-		        if (GetCurrentMana(myHero)/GetMaxMana(myHero)) > 0.4 then
 			        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,250,GetCastRange(myHero,_Q),70,true,true)
                                 if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target,GetCastRange(myHero,_Q)) and HarassConfig.HarassQ then 
                                 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-				elseif CanUseSpell(myHero, _E) == READY and ValidTarget(target, 250) and IsInDistance(target, 250) and HarassConfig.HarassE then
+				elseif CanUseSpell(myHero, _E) == READY and ValidTarget(target, 250) and HarassConfig.HarassE then
 				CastSpell(_E)
 				end
-                        end
-		end
 	end
-end)
-
-OnLoop(function(myHero)
-
-	
 end)
 
 function Killsteal()
