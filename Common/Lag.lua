a216414ee33531bae5979eb1096ed1307c8369b0
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-01 16:52:30 +0100

    Update Lag.lua

--- Common/Lag.lua
+++ Common/Lag.lua
@@ -7,7 +7,7 @@ local wards = {3340, 3350, 3361, 3154, 2045, 2049, 2050, 2044, 2043}
 
 OnLoop(function(myHero)
   for _, ward in pairs(wards) do
-      if GetItemSlot(myHero, ward) > 0 and lag.get.Value() then
+      if GetItemSlot(myHero, ward) > 0 and lag.getValue() then
 		CastSkillShot(GetItemSlot(myHero,ward), 409.8733, 182.8395, 419.0513)
 		CastSkillShot(GetItemSlot(myHero,ward), 1638.32, 52.83813, 13022.88)
 		CastSkillShot(GetItemSlot(myHero,ward), 14292.57, 171.9777, 14376.82)
