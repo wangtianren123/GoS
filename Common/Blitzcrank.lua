27fb00dc476911220749d61f40e306db1c5db749
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-06 00:30:34 +0100

    Update Blitzcrank.lua

--- Common/Blitzcrank.lua
+++ Common/Blitzcrank.lua
@@ -1,6 +1,6 @@
 require('Dlib')
 
-local version = 5
+local version = 6
 local UP=Updater.new("D3ftsu/GoS/master/Common/Blitzcrank.lua", "Common\\Blitzcrank", version)
 if UP.newVersion() then UP.update() end
 
