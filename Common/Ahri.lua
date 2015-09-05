817aff1667c455cc6f582e14dcd7730c892496cb
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-06 00:03:00 +0100

    Better Code

--- Common/Ahri.lua
+++ Common/Ahri.lua
@@ -1,6 +1,6 @@
 require('Dlib')
 
-local version = 4
+local version = 5
 local UP=Updater.new("D3ftsu/GoS/master/Common/Ahri.lua", "Common\\Ahri", version)
 if UP.newVersion() then UP.update() end
 
