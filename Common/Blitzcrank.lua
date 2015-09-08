2331187307a0623bb7da871df2a34a6ef67aa522
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-08 13:57:27 +0100

    Update Blitzcrank.lua

--- Common/Blitzcrank.lua
+++ Common/Blitzcrank.lua
@@ -1,59 +1,31 @@
-require('Dlib')
+BlitzcrankMenu = Menu("Blitzcrank", "Blitzcrank")
+BlitzcrankMenu:SubMenu("Combo", "Combo")
+BlitzcrankMenu.Combo:Boolean("Q", "Use Q", true)
+BlitzcrankMenu.Combo:Boolean("W", "Use W", true)
+BlitzcrankMenu.Combo:Boolean("E", "Use E", true)
+BlitzcrankMenu.Combo:Boolean("R", "Use R", true)
 
-local version = 7
-local UP=Updater.new("D3ftsu/GoS/master/Common/Blitzcrank.lua", "Common\\Blitzcrank", version)
-if UP.newVersion() then UP.update() end
+BlitzcrankMenu:SubMenu("Harass", "Harass")
+BlitzcrankMenu.Harass:Boolean("Q", "Use Q", true)
+BlitzcrankMenu.Harass:Boolean("E", "Use E", true)
+BlitzcrankMenu.Harass:Slider("Mana", "if Mana % is More than", 30, 0, 80, 1)
 
---------------- Thanks ilovesona for this ------------------------
-DelayAction(function ()
-        for _, imenu in pairs(menuTable) do
-                local submenu = menu.addItem(SubMenu.new(imenu.name))
-                for _,subImenu in pairs(imenu) do
-                        if subImenu.type == SCRIPT_PARAM_ONOFF then
-                                local ggeasy = submenu.addItem(MenuBool.new(subImenu.t, subImenu.value))
-                                OnLoop(function(myHero) subImenu.value = ggeasy.getValue() end)
-                        elseif subImenu.type == SCRIPT_PARAM_KEYDOWN then
-                                local ggeasy = submenu.addItem(MenuKeyBind.new(subImenu.t, subImenu.key))
-                                OnLoop(function(myHero) subImenu.key = ggeasy.getValue(true) end)
-                        elseif subImenu.type == SCRIPT_PARAM_INFO then
-                                submenu.addItem(MenuSeparator.new(subImenu.t))
-                        end
-                end
-        end
-        _G.DrawMenu = function ( ... )  end
-end, 1000)
+BlitzcrankMenu:SubMenu("Killsteal", "Killsteal")
+BlitzcrankMenu.Killsteal:Boolean("Q", "Killsteal with Q", true)
+BlitzcrankMenu.Killsteal:Boolean("R", "Killsteal with R", true)
 
-local root = menu.addItem(SubMenu.new("Blitzcrank"))
+BlitzcrankMenu:SubMenu("Misc", "Misc")
+BlitzcrankMenu.Misc:Boolean("Autoignite", "Auto Ignite", true)
+BlitzcrankMenu.Misc:Boolean("Autolvl", "Auto level", false)
+BlitzcrankMenu.Misc:Boolean("Interrupt", "Interrupt Dangerous Spells with E", true)
 
-local Combo = root.addItem(SubMenu.new("Combo"))
-local CUseQ = Combo.addItem(MenuBool.new("Use Q",true))
-local CUseW = Combo.addItem(MenuBool.new("Use W",true))
-local CUseE = Combo.addItem(MenuBool.new("Use E",true))
-local CUseR = Combo.addItem(MenuBool.new("Use R",true))
+BlitzcrankMenu:SubMenu("Junglesteal", "Baron/Drake Steal")
+BlitzcrankMenu.Junglesteal:Boolean("Q", "Use Q", true)
+BlitzcrankMenu.Junglesteal:Boolean("R", "Use R", true)
 
-local Harass = root.addItem(SubMenu.new("Harass"))
-local HUseQ = Harass.addItem(MenuBool.new("Use Q",true))
-local HUseE = Harass.addItem(MenuBool.new("Use E",true))
-local HMmana = Harass.addItem(MenuSlider.new("if My Mana % is More Than", 30, 0, 80, 1))
-
-local KSmenu = root.addItem(SubMenu.new("Killsteal"))
-local KSQ = KSmenu.addItem(MenuBool.new("Killsteal with Q", false))
-local KSR = KSmenu.addItem(MenuBool.new("Killsteal with R", false))
-
-local Misc = root.addItem(SubMenu.new("Misc"))
-local MiscAutolvl = Misc.addItem(SubMenu.new("Auto level", true))
-local MiscEnableAutolvl = MiscAutolvl.addItem(MenuBool.new("Enable", true))
-local MiscInterrupt = Misc.addItem(MenuBool.new("Interrupt", true))
-
-local JungleSteal = root.addItem(SubMenu.new("Baron/Drake Steal"))
-local JUseQ = JungleSteal.addItem(MenuBool.new("Use Q",true))
-local JUseR = JungleSteal.addItem(MenuBool.new("Use R",true))
-
-local Drawings = root.addItem(SubMenu.new("Drawings"))
-local DrawingsQ = Drawings.addItem(MenuBool.new("Draw Q Range", true))
-local DrawingsR = Drawings.addItem(MenuBool.new("Draw R Range", true))
-
-myIAC = IAC()
+BlitzcrankMenu:SubMenu("Drawings", "Drawings")
+BlitzcrankMenu.Drawings:Boolean("Q", "Draw Q Range", true)
+BlitzcrankMenu.Drawings:Boolean("R", "Draw R Range", true)
 
 CHANELLING_SPELLS = {
     ["Caitlyn"]                     = {_R},
@@ -83,63 +55,72 @@ OnProcessSpell(function(unit, spell)
 end)
  
 function addInterrupterCallback( callback0 )
-        callback = callback0
+callback = callback0
 end
 
 OnLoop(function(myHero)
-    if IWalkConfig.Combo then
-	local target = GetCurrentTarget()
-	              	
-		local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,550,975,80,true,true)
+    if IOW:Mode() == "Combo" then
+	
+	    local target = GetCurrentTarget() 	
+		local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1800,250,975,80,true,true)
 		
-        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, 975) and CUseQ.getValue() then
+        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 975) and BlitzcrankMenu.Combo.Q:Value() then
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	    end
                           
-        if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 800) and GetDistance(myHero, target) > 200 and CUseW.getValue() then
+        if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 800) and GoS:GetDistance(myHero, target) > 200 and BlitzcrankMenu.Combo.W:Value()
         CastSpell(_W)
 		end
 			
-        if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 250) and CUseE.getValue() then
+        if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 250) and BlitzcrankMenu.Combo.E:Value() then
         CastSpell(_E)
 		end
 		              
-		if CanUseSpell(myHero, _R) == READY and ValidTarget(target, 600) and CUseR.getValue() then
+		if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, 600) and BlitzcrankMenu.Combo.R:Value() and 100*GetCurrentHP(target)/GetMaxHP(target) < 80 then
         CastSpell(_R)
 	    end
 	                      
 	end	
 	
-	if IWalkConfig.Harass and (GetCurrentMana(myHero)/GetMaxMana(myHero))*100 >= HMmana.getValue() then
-	local target = GetCurrentTarget()
+	if IOW:Mode() == "Harass" and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= BlitzcrankMenu.Harass.Mana:Value() then
+	
+		local target = GetCurrentTarget()
+		local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1800,250,975,80,true,true)
 		
-		local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,550,975,80,true,true)
-		
-        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, 975) and HUseQ.getValue() then
+        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 975) and BlitzcrankMenu.Harass.Q:Value() then
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	    end
 		
-		if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 250) and HUseE.getValue() then
+		if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 250) and BlitzcrankMenu.Harass.E:Value() then
         CastSpell(_E)
 		end
+		
 	end
 	
-	for i,enemy in pairs(GetEnemyHeroes()) do
-	    local QPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),1800,550,975,80,true,true)
+	for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+	
+	    local QPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1800,250,975,80,true,true)
 		
 		local ExtraDmg = 0
 		if GotBuff(myHero, "itemmagicshankcharge") > 99 then
 		ExtraDmg = ExtraDmg + 0.1*GetBonusAP(myHero) + 100
 		end
 		
-  	    if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(enemy, 975) and KSQ.getValue() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then 
+		if Ignite and BlitzcrankMenu.Misc.Autoignite:Value() then
+            if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetHPRegen(enemy)*2.5 and GoS:GetDistanceSqr(GetOrigin(enemy)) < 600*600 then
+            CastTargetSpell(enemy, Ignite)
+            end
+        end
+		
+  	    if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 975) and BlitzcrankMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then 
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-        elseif CanUseSpell(myHero, _R) == READY and ValidTarget(enemy, 600) and KSR.getValue() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
+        elseif CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(enemy, 600) and BlitzcrankMenu.Killsteal.R:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
         CastSpell(_R)
 	    end
+		
 	end
 	
-if MiscEnableAutolvl.getValue() then  
+if BlitzcrankMenu.Misc.Autolvl:Value() then    
 
 if GetLevel(myHero) >= 1 and GetLevel(myHero) < 2 then
 	LevelSpell(_Q)
@@ -181,39 +162,34 @@ end
 
 end
 
-for _,mob in pairs(GetAllMinions(MINION_JUNGLE)) do
-	local mobPos = GetOrigin(mob)
+for _,mob in pairs(GoS:GetAllMinions(MINION_JUNGLE)) do
 	
+	local mobPos = GetOrigin(mob)
     local ExtraDmg = 0
 	if GotBuff(myHero, "itemmagicshankcharge") > 99 then
 	ExtraDmg = ExtraDmg + 0.1*GetBonusAP(myHero) + 100
 	end	
 	
 	if GetObjectName(mob) == "SRU_Dragon" or GetObjectName(mob) == "SRU_Baron" then
-	  if CanUseSpell(myHero, _Q) == READY and JUseQ.getValue() and ValidTarget(mob, 975) and GetCurrentHP(mob) < CalcDamage(myHero, mob, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then
+	  if CanUseSpell(myHero, _Q) == READY and BlitzcrankMenu.Junglesteal.Q:Value() and GoS:ValidTarget(mob, 975) and GetCurrentHP(mob) < GoS:CalcDamage(myHero, mob, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then
 	  CastSkillShot(_Q,mobPos.x, mobPos.y, mobPos.z)
 	  end
 		
-	  if CanUseSpell(myHero, _R) == READY and JUseR.getValue() and ValidTarget(mob, 600) and GetCurrentHP(mob) < CalcDamage(myHero, mob, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
+	  if CanUseSpell(myHero, _R) == READY and BlitzcrankMenu.Junglesteal.R:Value() and GoS:ValidTarget(mob, 600) and GetCurrentHP(mob) < GoS:CalcDamage(myHero, mob, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
 	  CastSpell(_R)
-          end
-        end
+      end
+    end
 end
 
-local HeroPos = GetOrigin(myHero)		
-if DrawingsQ.getValue() then DrawCircle(HeroPos.x,HeroPos.y,HeroPos.z,1000,3,100,0xff00ff00) end
-if DrawingsR.getValue() then DrawCircle(HeroPos.x,HeroPos.y,HeroPos.z,600,3,100,0xff00ff00) end
+if BlitzcrankMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,975,3,100,0xff00ff00) end
+if BlitzcrankMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,3,100,0xff00ff00) end
 end)
 
 addInterrupterCallback(function(target, spellType)
-local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1500,250,975,100,true,true)
-  if IsInDistance(target, 975) and CanUseSpell(myHero,_Q) == READY and MiscInterrupt.getValue() and spellType == CHANELLING_SPELLS then
+  local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1500,250,975,100,true,true)
+  if GoS:IsInDistance(target, 975) and CanUseSpell(myHero,_Q) == READY and BlitzcrankMenu.Misc.Interrupt:Value() and spellType == CHANELLING_SPELLS then
   CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-  elseif IsInDistance(target, 600) and CanUseSpell(myHero,_R) == READY and MiscInterrupt.getValue() and spellType == CHANELLING_SPELLS then
+  elseif GoS:IsInDistance(target, 600) and CanUseSpell(myHero,_R) == READY and BlitzcrankMenu.Misc.Interrupt:Value() and spellType == CHANELLING_SPELLS then
   CastSpell(_R)
   end
 end)
-
-AddGapcloseEvent(_Q, 975, false)
-
-notification("Blitzcrank by Deftsu loaded.", 10000)
