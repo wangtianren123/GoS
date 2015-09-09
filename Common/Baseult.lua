4c0d1b57024e0ece2aca1012c4467bb9eb24e3e1
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-09 04:49:54 +0100

    Update Baseult.lua

--- Common/Baseult.lua
+++ Common/Baseult.lua
@@ -25,26 +25,26 @@ end
 if GetObjectName(myHero) == "Ashe" then
 	delay = 250
 	missileSpeed = 1600
-	damage = function(target) return CalcDamage(myHero, target, 0, 75 + 175*GetCastLevel(myHero,_R) + GetBonusAP(myHero)) end
+	damage = function(target) return GoS:CalcDamage(myHero, target, 0, 75 + 175*GetCastLevel(myHero,_R) + GetBonusAP(myHero)) end
 elseif GetObjectName(myHero) == "Draven" then
 	delay = 400
 	missileSpeed = 2000
-	damage = function(target) return CalcDamage(myHero, target, 75 + 100*GetCastLevel(myHero,_R) + 1.1*GetBonusDmg(myHero)) end
+	damage = function(target) return GoS:CalcDamage(myHero, target, 75 + 100*GetCastLevel(myHero,_R) + 1.1*GetBonusDmg(myHero)) end
 elseif GetObjectName(myHero) == "Ezreal" then
 	delay = 1000
 	missileSpeed = 2000
-	damage = function(target) return CalcDamage(myHero, target, 0, 200 + 150*GetCastLevel(myHero,_R) + .9*GetBonusAP(myHero)+GetBonusDmg(myHero)) end
+	damage = function(target) return GoS:CalcDamage(myHero, target, 0, 200 + 150*GetCastLevel(myHero,_R) + .9*GetBonusAP(myHero)+GetBonusDmg(myHero)) end
 elseif GetObjectName(myHero) == "Jinx" then
 	delay = 600
-    missileSpeed = (GoS:GetDistance(enemyBasePos) / (1 + (GoS:GetDistance(enemyBasePos)-1500)/2500)) -- thanks Noddy
-	damage = function(target) return CalcDamage(myHero, target, (GetMaxHP(target)-GetCurrentHP(target))*(0.2+0.05*GetCastLevel(myHero, _R)) + 150 + 100*GetCastLevel(myHero,_R) + GetBonusDmg(myHero)) end
+        missileSpeed = (GoS:GetDistance(enemyBasePos) / (1 + (GoS:GetDistance(enemyBasePos)-1500)/2500)) -- thanks Noddy
+	damage = function(target) return GoS:CalcDamage(myHero, target, (GetMaxHP(target)-GetCurrentHP(target))*(0.2+0.05*GetCastLevel(myHero, _R)) + 150 + 100*GetCastLevel(myHero,_R) + GetBonusDmg(myHero)) end
 end
 
 OnProcessRecall(function(Object,recallProc)
 	if CanUseSpell(myHero, _R) == READY and BaseultMenu.Enabled:Value() and GetTeam(Object) ~= GetTeam(myHero) then
 		if damage(Object) > GetCurrentHP(Object) then
 			local timeToRecall = recallProc.totalTime
-			local distance = GetDistance(enemyBasePos)
+			local distance = GoS:GetDistance(enemyBasePos)
 			local timeToHit = delay + (distance * 1000 / missileSpeed)
 			if timeToRecall > timeToHit then
 				recallPos = Vector(Object)
