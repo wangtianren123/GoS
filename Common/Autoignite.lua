18f59272cdff5c6ca709c700f67d5e9d19ea29d2
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-13 21:59:18 +0100

    Update Autoignite.lua

--- Common/Autoignite.lua
+++ Common/Autoignite.lua
@@ -9,15 +9,15 @@ DuelSmite = (summonerNameOne:lower():find("s5_summonersmiteduel") and SUMMONER_1
 
 for i,enemy in pairs(GoS:GetEnemyHeroes()) do
       if ChillingSmite and AutoSmiteMenu.Enabled:Value() then
-                  if CanUseSpell(myHero, ChillingSmite) == READY and 20+8*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy) and GoS:GetDistanceSqr(GetOrigin(enemy)) < 500*500 then
+                  if CanUseSpell(myHero, ChillingSmite) == READY and 20+8*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy) and GoS:ValidTarget(enemy, 500) then
                   CastTargetSpell(enemy, ChillingSmite)
                   end
       elseif Ignite and AutoIgniteMenu.Enabled:Value() then
-                  if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:GetDistanceSqr(GetOrigin(enemy)) < 600*600 then
+                  if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
                   CastTargetSpell(enemy, Ignite)
                   end
       elseif DuelSmite and AutoSmiteMenu.Enabled:Value() then
-                  if CanUseSpell(myHero, DuelSmite) == READY and 54+6*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*3 and GoS:GetDistanceSqr(GetOrigin(enemy)) < 500*500 then
+                  if CanUseSpell(myHero, DuelSmite) == READY and 54+6*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*3 and GoS:ValidTarget(enemy, 500) then
                   CastTargetSpell(enemy, DuelSmite)
                   end
       end
