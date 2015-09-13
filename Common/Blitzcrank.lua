482ba6f6374e8d85309f7bd5b262ec89a63001d2
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-14 00:07:16 +0100

    Update Blitzcrank.lua

--- Common/Blitzcrank.lua
+++ Common/Blitzcrank.lua
@@ -60,48 +60,70 @@ function addInterrupterCallback( callback0 )
 callback = callback0
 end
 
+local Tick = 0
+
 OnLoop(function(myHero)
+Tick = Tick + 1
+Checks()
+Drawings()
+
+if Tick > 32 then
+Combo()
+Harass()
+Killsteal()
+Junglesteal()
+Autolvl()
+Tick = 0
+end
+
+end)
+
+function Combo()
     if IOW:Mode() == "Combo" then
 	
 	        local target = GetCurrentTarget()
 		local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1800,250,975,80,true,true)
 		
-                if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 975) and BlitzcrankMenu.Combo.Q:Value() then
+                if SpellQREADY and QPred.HitChance == 1 and GoS:ValidTarget(target, 975) and BlitzcrankMenu.Combo.Q:Value() then
                 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	        end
                           
-                if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 800) and GoS:GetDistance(myHero, target) > 200 and BlitzcrankMenu.Combo.W:Value() then
+                if SpellWREADY and GoS:ValidTarget(target, 800) and GoS:GetDistance(myHero, target) > 200 and BlitzcrankMenu.Combo.W:Value() then
                 CastSpell(_W)
 		end
 			
-                if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 250) and BlitzcrankMenu.Combo.E:Value() then
+                if SpellEREADY and GoS:ValidTarget(target, 250) and BlitzcrankMenu.Combo.E:Value() then
                 CastSpell(_E)
 		end
 		              
-		if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, 600) and BlitzcrankMenu.Combo.R:Value() and 100*GetCurrentHP(target)/GetMaxHP(target) < 80 then
+		if SpellRREADY and GoS:ValidTarget(target, 600) and BlitzcrankMenu.Combo.R:Value() and 100*GetCurrentHP(target)/GetMaxHP(target) < 80 then
                 CastSpell(_R)
 	        end
 	                      
 	end	
+end
 	
+function Harass()	
 	if IOW:Mode() == "Harass" and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= BlitzcrankMenu.Harass.Mana:Value() then
 	
 		local target = GetCurrentTarget()
 		local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1800,250,975,80,true,true)
 		
-                if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 975) and BlitzcrankMenu.Harass.Q:Value() then
+                if SpellQREADY and QPred.HitChance == 1 and GoS:ValidTarget(target, 975) and BlitzcrankMenu.Harass.Q:Value() then
                 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	        end
 		
-		if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 250) and BlitzcrankMenu.Harass.E:Value() then
+		if SpellEREADY and GoS:ValidTarget(target, 250) and BlitzcrankMenu.Harass.E:Value() then
                 CastSpell(_E)
 		end
 		
 	end
+end
 	
+function Killsteal()
 	for i,enemy in pairs(GoS:GetEnemyHeroes()) do
 	
-	    local QPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1800,250,975,80,true,true)
+	        local QPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1800,250,975,80,true,true)
 		
 		local ExtraDmg = 0
 		if GotBuff(myHero, "itemmagicshankcharge") > 99 then
@@ -109,19 +131,21 @@ OnLoop(function(myHero)
 		end
 		
 		if Ignite and BlitzcrankMenu.Misc.Autoignite:Value() then
-                  if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
+                  if SpellIREADY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
                   CastTargetSpell(enemy, Ignite)
                   end
                 end
 		
-  	        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 975) and BlitzcrankMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then 
+  	        if SpellQREADY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 975) and BlitzcrankMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then 
                 CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-                elseif CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(enemy, 600) and BlitzcrankMenu.Killsteal.R:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
+                elseif SpellRREADY and GoS:ValidTarget(enemy, 600) and BlitzcrankMenu.Killsteal.R:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
                 CastSpell(_R)
 	        end
 		
 	end
-	
+end
+
+function Autolvl()	
 if BlitzcrankMenu.Misc.Autolvl:Value() then    
 
 if GetLevel(myHero) >= 1 and GetLevel(myHero) < 2 then
@@ -163,7 +187,9 @@ elseif GetLevel(myHero) == 18 then
 end
 
 end
+end
 
+function Junglesteal()
 for _,mob in pairs(GoS:GetAllMinions(MINION_JUNGLE)) do
 	
 	local mobPos = GetOrigin(mob)
@@ -173,19 +199,29 @@ for _,mob in pairs(GoS:GetAllMinions(MINION_JUNGLE)) do
 	end	
 	
 	if GetObjectName(mob) == "SRU_Dragon" or GetObjectName(mob) == "SRU_Baron" then
-	  if CanUseSpell(myHero, _Q) == READY and BlitzcrankMenu.Junglesteal.Q:Value() and GoS:ValidTarget(mob, 975) and GetCurrentHP(mob) < GoS:CalcDamage(myHero, mob, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then
+	  if SpellQREADY and BlitzcrankMenu.Junglesteal.Q:Value() and GoS:ValidTarget(mob, 975) and GetCurrentHP(mob) < GoS:CalcDamage(myHero, mob, 0, 55*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero) + ExtraDmg) then
 	  CastSkillShot(_Q,mobPos.x, mobPos.y, mobPos.z)
 	  end
 		
-	  if CanUseSpell(myHero, _R) == READY and BlitzcrankMenu.Junglesteal.R:Value() and GoS:ValidTarget(mob, 600) and GetCurrentHP(mob) < GoS:CalcDamage(myHero, mob, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
+	  if SpellRREADY and BlitzcrankMenu.Junglesteal.R:Value() and GoS:ValidTarget(mob, 600) and GetCurrentHP(mob) < GoS:CalcDamage(myHero, mob, 0, 125*GetCastLevel(myHero,_R)+125+GetBonusAP(myHero) + ExtraDmg) then
 	  CastSpell(_R)
           end
         end
 end
+end
 
+function Drawings()
 if BlitzcrankMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,975,3,100,0xff00ff00) end
 if BlitzcrankMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,3,100,0xff00ff00) end
-end)
+end
+
+function Checks()
+SpellQREADY = CanUseSpell(myHero,_Q) == READY
+SpellWREADY = CanUseSpell(myHero,_W) == READY
+SpellEREADY = CanUseSpell(myHero,_E) == READY
+SpellRREADY = CanUseSpell(myHero,_R) == READY
+SpellIREADY = CanUseSpell(myHero,Ignite) == READY
+end 
 
 addInterrupterCallback(function(target, spellType)
   local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1500,250,975,100,true,true)
