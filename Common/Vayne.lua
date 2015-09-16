b09136e09afe003c216c7cce4795e3b7a68db86f
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-16 13:22:07 +0100

    Update Vayne.lua

--- Common/Vayne.lua
+++ Common/Vayne.lua
@@ -166,14 +166,6 @@ OnProcessSpell(function(unit, spell)
                         end
                 end, spell.windUpTime*1000 + GetLatency())
 	end		
-	
-        if spell.name:lower():find("vaynetumble") then 
-	        GoS:DelayAction(function() 
-                        if IOW:Mode() == "Combo" and VayneMenu.Combo.Q:Value() then
-                        AttackUnit(target)
-                        end
-                 end, spell.animationTime*1000 + GetLatency())
-        end
       end
   end
 end)
