a23d7ed260be49f1f7686c35791e559e7e885de5
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-16 13:11:27 +0100

    Update Ashe.lua

--- Common/Ashe.lua
+++ Common/Ashe.lua
@@ -57,24 +57,8 @@ function addInterrupterCallback( callback0 )
 callback = callback0
 end
 
-local Tick = 0
-
 OnLoop(function(myHero)
-Tick = Tick + 1
-Checks()
-Drawings()
 
-if Tick > 32 then
-Combo()
-Harass()
-Killsteal()
-Autolvl()
-Tick = 0
-end
-
-end)
- 
-function Combo()
     if IOW:Mode() == "Combo" then
 	
 	local target = GetCurrentTarget()
@@ -113,9 +97,7 @@ function Combo()
         CastTargetSpell(myHero, GetItemSlot(myHero,3139))
         end
     end
-end
 
-function Harass()
     if IOW:Mode() == "Harass" and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= AsheMenu.Harass.Mana:Value() then 
     
         local target = GetCurrentTarget()
@@ -130,9 +112,7 @@ function Harass()
 	end
 		
     end
-end
 
-function Killsteal()
     for i,enemy in pairs(GoS:GetEnemyHeroes()) do
 	
 	local WPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),2000,250,1200,50,true,true)
@@ -153,9 +133,7 @@ function Killsteal()
 	end
 		
     end
-end
 
-function Autolvl()
 if AsheMenu.Misc.Autolvl:Value() then  
 
 if GetLevel(myHero) >= 1 and GetLevel(myHero) < 2 then
@@ -197,19 +175,16 @@ elseif GetLevel(myHero) == 18 then
 end
 
 end
-end
 
-function Drawings()
 if AsheMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
-end
 
-function Checks()
 SpellQREADY = CanUseSpell(myHero,_Q) == READY
 SpellWREADY = CanUseSpell(myHero,_W) == READY
 SpellEREADY = CanUseSpell(myHero,_E) == READY
 SpellRREADY = CanUseSpell(myHero,_R) == READY
 SpellIREADY = CanUseSpell(myHero,Ignite) == READY
-end 
+
+end)
 
 addInterrupterCallback(function(target, spellType)
   local RPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1600,250,1000,130,false,true)
