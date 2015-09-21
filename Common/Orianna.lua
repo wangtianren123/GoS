d3bc9aedd86dcf38885c3c1aa3f8cfeb5385ddf1
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-21 11:48:32 +0100

    Update Orianna.lua

--- Common/Orianna.lua
+++ Common/Orianna.lua
@@ -37,26 +37,26 @@ OriannaMenu.Drawings:Boolean("E", "Draw E Range", true)
 
 OnLoop(function(myHero)
 
-	if Ball and GetDistance(myHero, Ball) > 1250 then
-    Ball = nil
-    end
+        if Ball and GoS:GetDistance(myHero, Ball) > 1250 then
+        Ball = nil
+        end
 	
 	if IOW:Mode() == "Combo" then
 	
-    local target = GetCurrentTarget()
+        local target = GetCurrentTarget()
 	local targetPos = GetOrigin(target)
-	local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1200,0,825,80,false,true)
+	local QPred = GetPredictionForPlayer(Ball or GoS:myHeroPos(),target,GetMoveSpeed(target),1200,0,825,80,false,true)
 
 	if SpellQREADY and QPred.HitChance == 1 and OriannaMenu.Combo.Q:Value() and GoS:ValidTarget(target, 825) then
-    CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)   
+        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)   
 	end
 	
 	if SpellWREADY and OriannaMenu.Combo.W:Value() and GoS:ValidTarget(target, 825) then
 	local BallPos = Ball or myHero
-	  if GoS:GetDistance(BallPos, target) < 245 then
-	  CastSpell(_W)
-      end
-    end
+	 if GoS:GetDistance(Ball or GoS:myHeroPos(), target) < 245 then
+	 CastSpell(_W)
+         end
+        end
 
     if Ball ~= nil and SpellEREADY and GoS:ValidTarget(target, 1000) and OriannaMenu.Combo.E:Value() then
       local x,i,z = VectorPointProjectionOnLineSegment(GoS:myHeroPos(), targetPos, Ball)
@@ -68,20 +68,22 @@ end
 	
 	if IOW:Mode() == "Harass" then
 	
-    local target = GetCurrentTarget()
+        local target = GetCurrentTarget()
 	local targetPos = GetOrigin(target)
 	local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1200,0,825,80,false,true)
 
+	local QPred = GetPredictionForPlayer(Ball or GoS:myHeroPos(),target,GetMoveSpeed(target),1200,0,825,80,false,true)
+
 	if SpellQREADY and QPred.HitChance == 1 and OriannaMenu.Harass.Q:Value() and GoS:ValidTarget(target, 825) then
-    CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)   
+        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)   
 	end
 	
 	if SpellWREADY and OriannaMenu.Harass.W:Value() and GoS:ValidTarget(target, 825) then
 	local BallPos = Ball or myHero
-	  if GoS:GetDistance(BallPos, target) < 245 then
-	  CastSpell(_W)
-          end
-       end
+	 if GoS:GetDistance(Ball or GoS:myHeroPos(), target) < 245 then
+	 CastSpell(_W)
+         end
+        end
 
     if Ball ~= nil and SpellEREADY and GoS:ValidTarget(target, 1000) and OriannaMenu.Harass.E:Value() then
       local x,i,z = VectorPointProjectionOnLineSegment(GoS:myHeroPos(), targetPos, Ball)
@@ -95,7 +97,7 @@ end)
 
 OnProcessSpell(function(unit, spell)
     if unit and spell and spell.name then
-      if unit == myHero then
+        if unit == myHero then
                 if spell.name:lower():find("OrianaIzunaCommand") then 
 		Ball = spell.endPos
 		end
@@ -103,12 +105,12 @@ OnProcessSpell(function(unit, spell)
 		if spell.name:lower():find("OrianaRedactCommand") then 
 		Ball = spell.target
 		end
-       end
+        end
     end
 end)
 
 OnCreateObj(function(Object) 
-if GetObjectBaseName(Object) == "TheDoomBall" and GetTeam(Object) == GetTeam(myHero) then
+if GetObjectBaseName(Object) == "TheDoomBall" then
 Ball = GetOrigin(Object)
 end
 end)
