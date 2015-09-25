fa226e5fa66a59f2cb863de1aa94765f3ba9ac21
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-25 21:30:21 +0100

    Update Sivir.lua

--- Common/Sivir.lua
+++ Common/Sivir.lua
@@ -9,6 +9,8 @@ SivirMenu.Combo:Boolean("R", "Use R", true)
 SivirMenu.Combo:Slider("Rmin", "Minimum Enemies in Range", 2, 1, 5, 1)
 SivirMenu.Combo:Slider("RMana", "R if Mana % >", 30, 0, 80, 1)
 SivirMenu.Combo:Boolean("Items", "Use Items", true)
+SivirMenu.Combo:Slider("myHP", "if HP % <", 50, 0, 100, 1)
+SivirMenu.Combo:Slider("targetHP", "if Target HP % >", 20, 0, 100, 1)
 SivirMenu.Combo:Boolean("QSS", "Use QSS", true)
 SivirMenu.Combo:Slider("QSSHP", "if My Health % <", 75, 0, 100, 1)
 
@@ -35,20 +37,8 @@ OnLoop(function(myHero)
 	local target = GetCurrentTarget()
 	local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1350,250,1075,85,false,true)
 	
-	if SpellQREADY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1075) and SivirMenu.Combo.Q:Value() and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= SivirMenu.Combo.QMana:Value() then
+	if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1075) and SivirMenu.Combo.Q:Value() and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= SivirMenu.Combo.QMana:Value() then
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-        end
-	
-	if GetItemSlot(myHero,3153) > 0 and SivirMenu.Combo.Items:Value() and GoS:ValidTarget(target, 550) and 100*GetCurrentHP(myHero)/GetMaxHP(myHero) < 50 and 100*GetCurrentHP(target)/GetMaxHP(target) > 20 then
-        CastTargetSpell(target, GetItemSlot(myHero,3153))
-        end
-
-        if GetItemSlot(myHero,3144) > 0 and SivirMenu.Combo.Items:Value() and GoS:ValidTarget(target, 550) and 100*GetCurrentHP(myHero)/GetMaxHP(myHero) < 50 and 100*GetCurrentHP(target)/GetMaxHP(target) > 20 then
-        CastTargetSpell(target, GetItemSlot(myHero,3144))
-        end
-
-        if GetItemSlot(myHero,3142) > 0 and SivirMenu.Combo.Items:Value() and GoS:ValidTarget(target, 600) then
-        CastTargetSpell(myHero, GetItemSlot(myHero,3142))
         end
 		
 	if GetItemSlot(myHero,3140) > 0 and SivirMenu.Combo.QSS:Value() and GotBuff(myHero, "rocketgrab2") > 0 or GotBuff(myHero, "charm") > 0 or GotBuff(myHero, "fear") > 0 or GotBuff(myHero, "flee") > 0 or GotBuff(myHero, "snare") > 0 or GotBuff(myHero, "taunt") > 0 or GotBuff(myHero, "suppression") > 0 or GotBuff(myHero, "stun") > 0 or GotBuff(myHero, "zedultexecute") > 0 or GotBuff(myHero, "summonerexhaust") > 0 and 100*GetCurrentHP(myHero)/GetMaxHP(myHero) < SivirMenu.Combo.QSSHP:Value() then
@@ -59,7 +49,7 @@ OnLoop(function(myHero)
         CastTargetSpell(myHero, GetItemSlot(myHero,3139))
         end
 		
-	if SpellRREADY and GoS:ValidTarget(target, 600) and SivirMenu.Combo.R:Value() and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= SivirMenu.Combo.RMana:Value() and GoS:EnemiesAround(GoS:myHeroPos(), 600) >= SivirMenu.Combo.Rmin:Value() then
+	if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, 600) and SivirMenu.Combo.R:Value() and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= SivirMenu.Combo.RMana:Value() and GoS:EnemiesAround(GoS:myHeroPos(), 600) >= SivirMenu.Combo.Rmin:Value() then
 	CastSpell(_R)
 	end
 
@@ -70,7 +60,7 @@ OnLoop(function(myHero)
         local target = GetCurrentTarget()
  	local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1350,250,1075,85,false,true)
 	
-	if SpellQREADY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1075) and SivirMenu.Harass.Q:Value() then
+	if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1075) and SivirMenu.Harass.Q:Value() then
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
         end
 
@@ -80,13 +70,25 @@ OnLoop(function(myHero)
 	
 	local QPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1350,250,1075,85,false,true)
 		
+	if GetItemSlot(myHero,3153) > 0 and SivirMenu.Combo.Items:Value() and GoS:ValidTarget(enemy, 550) and 100*GetCurrentHP(myHero)/GetMaxHP(myHero) < SivirMenu.Combo.myHP:Value() and 100*GetCurrentHP(enemy)/GetMaxHP(enemy) > SivirMenu.Combo.targetHP:Value() then
+        CastTargetSpell(enemy, GetItemSlot(myHero,3153))
+        end
+
+        if GetItemSlot(myHero,3144) > 0 and SivirMenu.Combo.Items:Value() and GoS:ValidTarget(enemy, 550) and 100*GetCurrentHP(myHero)/GetMaxHP(myHero) < SivirMenu.Combo.myHP:Value() and 100*GetCurrentHP(enemy)/GetMaxHP(enemy) > SivirMenu.Combo.targetHP:Value() then
+        CastTargetSpell(enemy, GetItemSlot(myHero,3144))
+        end
+
+        if GetItemSlot(myHero,3142) > 0 and SivirMenu.Combo.Items:Value() and GoS:ValidTarget(enemy, 600) then
+        CastTargetSpell(myHero, GetItemSlot(myHero,3142))
+        end
+        
 	if Ignite and SivirMenu.Misc.AutoIgnite:Value() then
-          if SpellIREADY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
+          if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
           CastTargetSpell(enemy, Ignite)
           end
 	end
 	
-	if SpellQREADY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 1075) and SivirMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 20*GetCastLevel(myHero,_Q)+5+(0.6+0.1*GetCastLevel(myHero, _Q))*(GetBaseDamage(myHero)+GetBonusDmg(myHero)) + 0.5*GetBonusAP(myHero), 0) then 
+	if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 1075) and SivirMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 20*GetCastLevel(myHero,_Q)+5+(0.6+0.1*GetCastLevel(myHero, _Q))*(GetBaseDamage(myHero)+GetBonusDmg(myHero)) + 0.5*GetBonusAP(myHero), 0) then 
 	CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 	end
 		
@@ -102,12 +104,6 @@ end
 
 if SivirMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x,GoS:myHeroPos().y,GoS:myHeroPos().z,1075,3,100,0xff00ff00) end
 
-SpellQREADY = CanUseSpell(myHero,_Q) == READY
-SpellWREADY = CanUseSpell(myHero,_W) == READY
---SpellEREADY = CanUseSpell(myHero,_E) == READY
-SpellRREADY = CanUseSpell(myHero,_R) == READY
-SpellIREADY = CanUseSpell(myHero,Ignite) == READY
-
 end)
 
 OnProcessSpell(function(unit, spell)
@@ -117,11 +113,11 @@ OnProcessSpell(function(unit, spell)
         if spell.name:lower():find("attack") then 
 	        GoS:DelayAction(function() 
 
-                                 if IOW:Mode() == "Combo" and GoS:ValidTarget(target, 600) and SivirMenu.Combo.W:Value() then	  
+                                 if IOW:Mode() == "Combo" and GoS:ValidTarget(target, 600) and CanUseSpell(myHero, _W) == READY and SivirMenu.Combo.W:Value() then	  
                                  CastSpell(_W)		
  			         end
                                                 
-                                 if IOW:Mode() == "Harass" and GoS:ValidTarget(target, 600) and SivirMenu.Harass.W:Value() and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= SivirMenu.Harass.Mana:Value() then	  
+                                 if IOW:Mode() == "Harass" and GoS:ValidTarget(target, 600) and CanUseSpell(myHero, _W) == READY and SivirMenu.Harass.W:Value() and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= SivirMenu.Harass.Mana:Value() then	  
                                  CastSpell(_W)
                                  end
                        
