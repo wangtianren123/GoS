3c4ec5d52dacb9943ef5bb0c3179188ca1b07fd6
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-26 18:09:07 +0100

    Update Orianna.lua

--- Common/Orianna.lua
+++ Common/Orianna.lua
@@ -8,10 +8,11 @@ OriannaMenu.Combo:Boolean("Q", "Use Q", true)
 OriannaMenu.Combo:Boolean("W", "Use W", true)
 OriannaMenu.Combo:Boolean("E", "Use E", true)
 OriannaMenu.Combo:SubMenu("R", "Use R")
-OriannaMenu.Combo.R:Slider("Rcatch", "if can catch X enemies", 2, 0, 5, 1)
+OriannaMenu.Combo.R:Boolean("REnabled", "Enabled", true)
 OriannaMenu.Combo.R:Boolean("Rkill", "if Can Kill", true)
-OriannaMenu.Combo.R:Key("FlashR", "R Flash Combo", string.byte("G"))
-OriannaMenu.Combo.R:Slider("FlashRcatch", "if can catch X enemies", 3, 0, 5, 1)
+OriannaMenu.Combo.R:Slider("Rcatch", "if can catch X enemies", 2, 0, 5, 1)
+--OriannaMenu.Combo.R:Key("FlashR", "R Flash Combo", string.byte("G"))
+--OriannaMenu.Combo.R:Slider("FlashRcatch", "if can catch X enemies", 3, 0, 5, 1)
 
 OriannaMenu:SubMenu("Harass", "Harass")
 OriannaMenu.Harass:Boolean("Q", "Use Q", true)
@@ -29,6 +30,7 @@ OriannaMenu.Misc:Boolean("AutoIgnite", "Auto Ignite", true)
 OriannaMenu.Misc:Boolean("Autolvl", "Auto level", true)
 OriannaMenu.Misc:List("Autolvltable", "Priority", 1, {"Q-W-E", "W-Q-E", "Q-E-W"})
 OriannaMenu.Misc:SubMenu("AutoUlt", "Auto Ult")
+OriannaMenu.Misc.AutoUlt:Boolean("Enabled", "Enabled", true)
 OriannaMenu.Misc.AutoUlt:Slider("1", "if Can Catch X Enemies", 3, 0, 5, 1)
 OriannaMenu.Misc.AutoUlt:Slider("2", "if Can Kill X Enemies", 2, 0, 5, 1)
 
@@ -36,6 +38,10 @@ OriannaMenu:SubMenu("JungleClear", "JungleClear")
 OriannaMenu.JungleClear:Boolean("Q", "Use Q", true)
 OriannaMenu.JungleClear:Boolean("W", "Use W", true)
 OriannaMenu.JungleClear:Boolean("E", "Use E", true)
+
+--[[OriannaMenu:SubMenu("LaneClear", "LaneClear")
+OriannaMenu.LaneClear:Boolean("Q", "Use Q", true)
+OriannaMenu.LaneClear:Boolean("W", "Use W", true)]]
 
 OriannaMenu:SubMenu("Drawings", "Drawings")
 OriannaMenu.Drawings:Boolean("Q", "Draw Q Range", true)
@@ -72,18 +78,24 @@ end
 
 OnLoop(function(myHero)
 	
-	if IOW:Mode() == "Combo" then
+     if IOW:Mode() == "Combo" then
 	
         local target = GetCurrentTarget()
 	local targetPos = GetOrigin(target)
 	local QPred = GetPredictionForPlayer(GetOrigin(Ball) or GoS:myHeroPos(),target,GetMoveSpeed(target),1200,0,825,80,false,true)
 
+	if CanUseSpell(myHero, _R) == READY and OriannaMenu.Combo.R.REnabled:Value() then
+	  if GoS:EnemiesAround(Ball or GoS:myHeroPos(), 400) >= OriannaMenu.Combo.R.Rcatch:Value() then
+	  CastSpell(_R)
+	  end
+	end
+	
 	if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and OriannaMenu.Combo.Q:Value() and GoS:ValidTarget(target, 825) then
         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)   
 	end
 	
-	if CanUseSpell(myHero, _W) == READY and OriannaMenu.Combo.W:Value() and GoS:ValidTarget(target, 825) then
-	 if GoS:GetDistance(Ball or GoS:myHeroPos(), target) < 250 then
+	if CanUseSpell(myHero, _W) == READY and OriannaMenu.Combo.W:Value() and GoS:ValidTarget(target, 1200) then
+	 if GoS:GetDistance(Ball or GoS:myHeroPos(), target) <= 250 then
 	 CastSpell(_W)
          end
         end
@@ -94,9 +106,9 @@ OnLoop(function(myHero)
           CastTargetSpell(myHero, _E)
           end
         end	
-end
+     end
 	
-	if IOW:Mode() == "Harass" then
+     if IOW:Mode() == "Harass" and 100*GetCurrentMana(myHero)/GetMaxMana(myHero) >= OriannaMenu.Harass.Mana:Value() then
 	
         local target = GetCurrentTarget()
 	local targetPos = GetOrigin(target)
@@ -107,19 +119,106 @@ end
 	end
 	
 	if CanUseSpell(myHero, _W) == READY and OriannaMenu.Harass.W:Value() and GoS:ValidTarget(target, 825) then
-	local BallPos = Ball or myHero
-	 if GoS:GetDistance(Ball or GoS:myHeroPos(), target) < 250 then
+	 if GoS:GetDistance(Ball or GoS:myHeroPos(), target) <= 250 then
 	 CastSpell(_W)
          end
         end
 
         if Ball ~= nil and CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 1000) and OriannaMenu.Harass.E:Value() then
           local pointSegment,pointLine,isOnSegment  = VectorPointProjectionOnLineSegment(GoS:myHeroPos(), targetPos, Vector(Ball))
-          if pointLine and GoS:GetDistance(pointSegment, target) < 80 then
+          if pointLine and GoS:GetDistance(pointSegment, target) <= 80 then
           CastTargetSpell(myHero, _E)
           end
         end	
+     end
+    
+	local KillableEnemies = 0
+	
+        for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+	  
+	    local QPred = GetPredictionForPlayer(GetOrigin(Ball) or GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1200,0,825,80,false,true)
+	    local enemyPos = GetOrigin(enemy)
+		
+	    local ExtraDmg = 0
+	    if GotBuff(myHero, "itemmagicshankcharge") > 99 then
+      	    ExtraDmg = ExtraDmg + 0.1*GetBonusAP(myHero) + 100
+	    end
+		
+	    if Ignite and OriannaMenu.Misc.AutoIgnite:Value() then
+              if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
+              CastTargetSpell(enemy, Ignite)
+              end
+            end
+						
+  	    if IOW:Mode() == "Combo" and GoS:ValidTarget(enemy, 1200) and OriannaMenu.Combo.R.Rkill:Value() and GoS:GetDistance(Ball or GoS:myHeroPos(), enemy) < 400 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 75*GetCastLevel(myHero, _R)+75+0.7*GetBonusAP(myHero) + ExtraDmg) then 
+            CastSpell(_R)
+            end
+		
+	    if CanUseSpell(myHero, _R) == READY and OriannaMenu.Misc.AutoUlt.Enabled:Value() then
+              if GoS:ValidTarget(enemy, 1200) and GoS:GetDistance(Ball or GoS:myHeroPos(), enemy) <= 400 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 75*GetCastLevel(myHero, _R)+75+0.7*GetBonusAP(myHero) + ExtraDmg) then 
+              KillableEnemies = KillableEnemies	+ 1
+              end
+		  
+	      if KillableEnemies >= OriannaMenu.Misc.AutoUlt.2:Value() then
+	      CastSpell(_R)
+	      end
+	    end
+		
+	    if CanUseSpell(myHero, _W) == READY and OriannaMenu.Killsteal.W:Value() and GoS:ValidTarget(target, 1200) and GoS:GetDistance(Ball or GoS:myHeroPos(), enemy) <= 250 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 45*GetCastLevel(myHero,_W)+25+0.7*GetBonusAP(myHero) + ExtraDmg) then
+	    CastSpell(_W)
+	    elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 825) and OriannaMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 30*GetCastLevel(myHero, _Q)+30+0.5*GetBonusAP(myHero) + ExtraDmg) then 
+            CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+            elseif CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(enemy, 1000) and OriannaMenu.Killsteal.E:Value() and Ball ~= nil then
+              local pointSegment,pointLine,isOnSegment  = VectorPointProjectionOnLineSegment(GoS:myHeroPos(), enemyPos, Vector(Ball))
+              if pointLine and GoS:GetDistance(pointSegment, enemy) <= 80 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 30*GetCastLevel(myHero, _R)+30+0.3GetBonusAP(myHero) + ExtraDmg) then
+              CastTargetSpell(myHero, _E)
+              end 
+	    end
+		
+	end
+	
+	if CanUseSpell(myHero, _R) == READY and OriannaMenu.Misc.AutoUlt.Enabled:Value() then
+	  if GoS:EnemiesAround(Ball or GoS:myHeroPos(), 400) >= OriannaMenu.Misc.AutoUlt.1:Value() then
+	  CastSpell(_R)
+	  end
+	end
+	
+        for _,mob in pairs(GoS:GetAllMinions(MINION_JUNGLE)) do
+		
+            if IOW:Mode() == "LaneClear" then
+	        local mobPos = GetOrigin(mob)
+	    
+		if CanUseSpell(myHero, _W) == READY and OriannaMenu.JungleClear.W:Value() and GoS:ValidTarget(mob, 1200) and GoS:GetDistance(Ball or GoS:myHeroPos(), mob) <= 250 then
+		CastSpell(_W)
+		end
+		
+		if CanUseSpell(myHero, _Q) == READY and OriannaMenu.JungleClear.Q:Value() and GoS:ValidTarget(mob, 825) then
+		CastSkillShot(_Q, mobPos.x, mobPos.y, mobPos.z) 
+		end
+		
+		if CanUseSpell(myHero, _E) == READY and OriannaMenu.JungleClear.E:Value() and GoS:ValidTarget(mob, 1000) and Ball ~= nil then
+		  local pointSegment,pointLine,isOnSegment  = VectorPointProjectionOnLineSegment(GoS:myHeroPos(), mobPos, Vector(Ball))
+                  if pointLine and GoS:GetDistance(pointSegment, mob) <= 80
+		  CastTargetSpell(myHero, _E)
+		  end
+		end
+            end
+	  
+        end
+	
+if OriannaMenu.Misc.Autolvl:Value() then  
+    if OriannaMenu.Misc.Autolvltable:Value() == 1 then leveltable = {_Q, _W, _E, _Q, _Q, _R, _Q, _W, _Q, _W, _R, _W, _W, _E, _E, _R, _E, _E}
+    elseif OriannaMenu.Misc.Autolvltable:Value() == 2 then leveltable = {_Q, _W, _E, _W, _W, _R, _W, _Q, _W, _Q, _R, _Q, _Q, _E, _E, _R, _E, _E}
+    elseif OriannaMenu.Misc.Autolvltable:Value() == 3 then leveltable = {_Q, _W, _E, _Q, _Q, _R, _Q, _E, _Q, _E, _R, _E, _E, _W, _W, _R, _W, _W}
+    end
+LevelSpell(leveltable[GetLevel(myHero)])
 end
+
+if OriannaMenu.Drawings.Ball:Value() then DrawCircle(GetOrigin(Ball).x or GoS:myHeroPos().x, GetOrigin(Ball).y or GoS:myHeroPos().y, GetOrigin(Ball).z or GoS:myHeroPos().z,150,0,1,0xffffffff) end
+if OriannaMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,825,0,1,0xff00ff00) end
+if OriannaMenu.Drawings.W:Value() then DrawCircle(GetOrigin(Ball).x or GoS:myHeroPos().x, GetOrigin(Ball).y or GoS:myHeroPos().y, GetOrigin(Ball).z or GoS:myHeroPos().z,250,0,1,0xff00ff00) end
+if OriannaMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,1000,0,1,0xff00ff00) end
+if OriannaMenu.Drawings.R:Value() then DrawCircle(GetOrigin(Ball).x or GoS:myHeroPos().x, GetOrigin(Ball).y or GoS:myHeroPos().y, GetOrigin(Ball).z or GoS:myHeroPos().z,400,0,1,0xff00ff00) end
 
 end)
 
@@ -142,6 +241,7 @@ OnCreateObj(function(Object)
 if GetObjectBaseName(Object) == "Orianna_Ball_Flash_Reverse" then
 Ball = nil
 end
+
 if GetObjectBaseName(Object) == "yomu_ring_green" then
 Ball = Object
 end
