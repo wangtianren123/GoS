1634624f230da9aa07debf2fe28c9a02524202c9
D3ftsu <ahmedtayachon1@gmail.com>
2015-09-26 14:31:58 +0100

    Update Ashe.lua

--- Common/Ashe.lua
+++ Common/Ashe.lua
@@ -146,7 +146,7 @@ if AsheMenu.Misc.Autolvl:Value() then
 LevelSpell(leveltable[GetLevel(myHero)])
 end
 
-if AsheMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,1200,3,100,0xff00ff00) end
+if AsheMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,1200,0,1,0xff00ff00) end
 
 end)
 
