f306dfc482a07c6d0c189519a47579b5d29e5148
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-13 12:14:57 +0100

    Create SpellData.lua

---
+++ Common/SpellData.lua
@@ -0,0 +1,8 @@
+return {
+	["Ahri"] = {
+		[_Q] = { Name = "AhriOrbofDeception", Range = 880, Speed = 2500, Delay = 250, Width = 100, collision = false, AOE = false, type = "linear"}
+		[_W] = { Name = "AhriFoxFire", Range = 700},
+		[_E] = { Name = "AhriSeduce", Range = 1000, Speed = 1550, Delay = 250,  Width = 60, collision = true, AOE = false, type = "linear"},
+		[_R] = { Name = "AhriTumble", Range = 550}
+	}
+}
