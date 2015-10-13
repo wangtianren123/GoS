c9e65c824f7f7909edd1f2bcc461d8be9eedce7a
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-13 12:17:53 +0100

    Update SpellData.lua

--- Common/SpellData.lua
+++ Common/SpellData.lua
@@ -1,8 +1,8 @@
 return {
 	["Ahri"] = {
-		[_Q] = { Name = "AhriOrbofDeception", Range = 880, Speed = 2500, Delay = 250, Width = 100, collision = false, AOE = false, type = "linear"}
+		[_Q] = { Name = "AhriOrbofDeception", Range = 1000, Speed = 2500, Delay = 250, Width = 100, collision = false, AOE = false, type = "linear", IsDangerous = false}
 		[_W] = { Name = "AhriFoxFire", Range = 700},
-		[_E] = { Name = "AhriSeduce", Range = 1000, Speed = 1550, Delay = 250,  Width = 60, collision = true, AOE = false, type = "linear"},
+		[_E] = { Name = "AhriSeduce", Range = 1000, Speed = 1550, Delay = 250,  Width = 60, collision = true, AOE = false, type = "linear", IsDangerous = true},
 		[_R] = { Name = "AhriTumble", Range = 550}
 	}
 }
