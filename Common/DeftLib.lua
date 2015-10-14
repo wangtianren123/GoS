75cdbf674809fce3418edc2776ed20afec85640e
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-14 10:21:35 +0100

    Update DeftLib.lua

--- Common/DeftLib.lua
+++ Common/DeftLib.lua
@@ -2,7 +2,7 @@ SpellData = {
   ["Aatrox"] = {
 		[_Q]  = { Name = "AatroxQ", ProjectileName = "AatroxQ.troy", Range = 650, Speed = 2000, Delay = 600, Width = 250, collision = false, type = "circular", IsDangerous = true}
 		[_E]  = { Name = "AatroxE", ProjectileName = "AatroxBladeofTorment_mis.troy" , Range = 1075, Speed = 1250, Delay = 250, Width = 35, collision = false, type = "linear", IsDangerous = false}
-	        [_R]  = { Name = "AatroxR", Range = 300} -- TODO : recheck radius
+	        [_R]  = { Name = "AatroxR", Range = 300}
 	}
   ["Ahri"] = {
 		[_Q]  = { Name = "AhriOrbofDeception", ProjectileName = "Ahri_Orb_mis.troy", Range = 1000, Speed = 2500, Delay = 250, Width = 100, collision = false, type = "linear", IsDangerous = false}
