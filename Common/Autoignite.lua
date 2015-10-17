85656397abcb9ea230770c17ef5321d36c136c03
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-17 15:10:07 +0100

    Update Autoignite.lua

--- Common/Autoignite.lua
+++ Common/Autoignite.lua
@@ -1,24 +1,27 @@
+require('Deftlib')
+
+if Ignite ~= nil then 
 local AutoIgniteMenu = Menu("AutoIgnite", "Auto Ignite")
 AutoIgniteMenu:Boolean("Enabled", "Enabled", true)
+end
 
+if Smite -= nil then 
 local AutoSmiteMenu = Menu("AutoSmite", "Auto Smite")
 AutoSmiteMenu:Boolean("Enabled", "Enabled", true)
-
-ChillingSmite = (GetCastName(myHero,SUMMONER_1):lower():find("s5_summonersmiteplayerganker") and SUMMONER_1 or (GetCastName(myHero,SUMMONER_2):lower():find("s5_summonersmiteplayerganker") and SUMMONER_2 or nil))
-DuelSmite = (GetCastName(myHero,SUMMONER_1):lower():find("s5_summonersmiteduel") and SUMMONER_1 or (GetCastName(myHero,SUMMONER_2):lower():find("s5_summonersmiteduel") and SUMMONER_2 or nil))
+end
 
 for i,enemy in pairs(GoS:GetEnemyHeroes()) do
-      if ChillingSmite and AutoSmiteMenu.Enabled:Value() then
-                  if CanUseSpell(myHero, ChillingSmite) == READY and 20+8*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy) and GoS:ValidTarget(enemy, 500) then
-                  CastTargetSpell(enemy, ChillingSmite)
+      if SmiteBlue and AutoSmiteMenu.Enabled:Value() then
+                  if CanUseSpell(myHero, SmiteBlue) == READY and 20+8*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy) and GoS:ValidTarget(enemy, 500) then
+                  CastTargetSpell(enemy, SmiteBlue)
                   end
       elseif Ignite and AutoIgniteMenu.Enabled:Value() then
                   if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
                   CastTargetSpell(enemy, Ignite)
                   end
-      elseif DuelSmite and AutoSmiteMenu.Enabled:Value() then
-                  if CanUseSpell(myHero, DuelSmite) == READY and 54+6*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*3 and GoS:ValidTarget(enemy, 500) then
-                  CastTargetSpell(enemy, DuelSmite)
+      elseif SmiteRed and AutoSmiteMenu.Enabled:Value() then
+                  if CanUseSpell(myHero, SmiteRed) == READY and 54+6*GetLevel(myHero) > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*3 and GoS:ValidTarget(enemy, 500) then
+                  CastTargetSpell(enemy, SmiteRed)
                   end
       end
 end
