505fa47173577fce3de384f1c1b6ddf9acd5c9c5
D3ftsu <ahmedtayachon1@gmail.com>
2015-10-25 13:20:51 +0100

    IOW V2

--- Common/Orianna.lua
+++ Common/Orianna.lua
@@ -1,9 +1,11 @@
 if GetObjectName(myHero) ~= "Orianna" then return end
 
+require('Deftlib')
+
 local Ball = myHero
 	
-local OriannaMenu = Menu("Orianna", "Orianna")
-OriannaMenu:SubMenu("Combo", "Combo")
+local OriannaMenu = MenuConfig("Orianna", "Orianna")
+OriannaMenu:Menu("Combo", "Combo")
 OriannaMenu.Combo:Boolean("Q", "Use Q", true)
 OriannaMenu.Combo:Boolean("W", "Use W", true)
 OriannaMenu.Combo:Boolean("E", "Use E", true)
@@ -14,13 +16,13 @@ OriannaMenu.Combo.R:Slider("Rcatch", "if can catch X enemies", 2, 0, 5, 1)
 --OriannaMenu.Combo.R:Key("FlashR", "R Flash Combo", string.byte("G"))
 --OriannaMenu.Combo.R:Slider("FlashRcatch", "if can catch X enemies", 3, 0, 5, 1)
 
-OriannaMenu:SubMenu("Harass", "Harass")
+OriannaMenu:Menu("Harass", "Harass")
 OriannaMenu.Harass:Boolean("Q", "Use Q", true)
 OriannaMenu.Harass:Boolean("W", "Use W", true)
 OriannaMenu.Harass:Boolean("E", "Use E", false)
 OriannaMenu.Harass:Slider("Mana", "if Mana % is More than", 30, 0, 80, 1)
 
-OriannaMenu:SubMenu("Killsteal", "Killsteal")
+OriannaMenu:Menu("Killsteal", "Killsteal")
 OriannaMenu.Killsteal:Boolean("Q", "Killsteal with Q", false)
 OriannaMenu.Killsteal:Boolean("W", "Killsteal with W", true)
 OriannaMenu.Killsteal:Boolean("E", "Killsteal with E", false)
@@ -35,49 +37,31 @@ OriannaMenu.Misc.AutoUlt:Boolean("Enabled", "Enabled", true)
 OriannaMenu.Misc.AutoUlt:Slider("catchable", "if Can Catch X Enemies", 3, 0, 5, 1)
 OriannaMenu.Misc.AutoUlt:Slider("killable", "if Can Kill X Enemies", 2, 0, 5, 1)
 
-OriannaMenu:SubMenu("JungleClear", "JungleClear")
+OriannaMenu:Menu("JungleClear", "JungleClear")
 OriannaMenu.JungleClear:Boolean("Q", "Use Q", true)
 OriannaMenu.JungleClear:Boolean("W", "Use W", true)
 OriannaMenu.JungleClear:Boolean("E", "Use E", true)
 
---[[OriannaMenu:SubMenu("LaneClear", "LaneClear")
+OriannaMenu:Menu("LaneClear", "LaneClear")
 OriannaMenu.LaneClear:Boolean("Q", "Use Q", true)
-OriannaMenu.LaneClear:Boolean("W", "Use W", true)]]
+OriannaMenu.LaneClear:Boolean("W", "Use W", true)
 
-OriannaMenu:SubMenu("Drawings", "Drawings")
+OriannaMenu:Menu("Drawings", "Drawings")
 OriannaMenu.Drawings:Boolean("Q", "Draw Q Range", true)
 OriannaMenu.Drawings:Boolean("W", "Draw W Radius", true)
 OriannaMenu.Drawings:Boolean("E", "Draw E Range", true)
 OriannaMenu.Drawings:Boolean("R", "Draw R Radius", true)
 OriannaMenu.Drawings:Boolean("Ball", "Draw Ball Position", true)
 
-CHANELLING_SPELLS = {
-    ["Katarina"]                    = {_R},
-    ["FiddleSticks"]                = {_R},
-    ["VelKoz"]                      = {_R},
-    ["Malzahar"]                    = {_R},
-    ["Warwick"]                     = {_R},
-}
-
-local callback = nil
- 
-OnProcessSpell(function(unit, spell)    
-    if not callback or not unit or GetObjectType(unit) ~= Obj_AI_Hero  or GetTeam(unit) == GetTeam(GetMyHero()) then return end
-    local unitChanellingSpells = CHANELLING_SPELLS[GetObjectName(unit)]
- 
-        if unitChanellingSpells then
-            for _, spellSlot in pairs(unitChanellingSpells) do
-                if spell.name == GetCastName(unit, spellSlot) then callback(unit, CHANELLING_SPELLS) end
-            end
-	end
+OnDraw(funtion(myHero)
+if OriannaMenu.Drawings.Ball:Value() then DrawCircle(GetOrigin(Ball),150,1,128,0xffffffff) end
+if OriannaMenu.Drawings.W:Value() then DrawCircle(GetOrigin(Ball),250,1,128,0xffffffff) end
+if OriannaMenu.Drawings.R:Value() then DrawCircle(GetOrigin(Ball),400,1,128,0xffffffff) end
+if OriannaMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos(),825,1,128,0xff00ff00) end
+if OriannaMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos(),1000,1,128,0xff00ff00) end
 end)
- 
-function addInterrupterCallback( callback0 )
-        callback = callback0
-end
 
-
-OnLoop(function(myHero)
+OnTick(function(myHero)
 	
      if IOW:Mode() == "Combo" then
 	    
@@ -146,23 +130,18 @@ OnLoop(function(myHero)
 	    local QPred = GetPredictionForPlayer(GetOrigin(Ball),enemy,GetMoveSpeed(enemy),1200,0,825,80,false,true)
 	    local enemyPos = GetOrigin(enemy)
 		
-	    local ExtraDmg = 0
-	    if GotBuff(myHero, "itemmagicshankcharge") > 99 then
-      	    ExtraDmg = ExtraDmg + 0.1*GetBonusAP(myHero) + 100
-	    end
-		
 	    if Ignite and OriannaMenu.Misc.AutoIgnite:Value() then
               if CanUseSpell(myHero, Ignite) == READY and 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetDmgShield(enemy)+GetHPRegen(enemy)*2.5 and GoS:ValidTarget(enemy, 600) then
               CastTargetSpell(enemy, Ignite)
               end
             end
 						
-  	    if IOW:Mode() == "Combo" and GoS:ValidTarget(enemy, 1200) and OriannaMenu.Combo.R.Rkill:Value() and GoS:GetDistance(Ball, enemy) < 400 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 75*GetCastLevel(myHero, _R)+75+0.7*GetBonusAP(myHero) + ExtraDmg) then 
+  	    if IOW:Mode() == "Combo" and GoS:ValidTarget(enemy, 1200) and OriannaMenu.Combo.R.Rkill:Value() and GoS:GetDistance(Ball, enemy) < 400 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 75*GetCastLevel(myHero, _R)+75+0.7*GetBonusAP(myHero) + Ludens()) then 
             CastSpell(_R)
             end
 		
 	    if CanUseSpell(myHero, _R) == READY and OriannaMenu.Misc.AutoUlt.Enabled:Value() then
-              if GoS:ValidTarget(enemy, 1200) and GoS:GetDistance(Ball, enemy) <= 400 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 75*GetCastLevel(myHero, _R)+75+0.7*GetBonusAP(myHero) + ExtraDmg) then 
+              if GoS:ValidTarget(enemy, 1200) and GoS:GetDistance(Ball, enemy) <= 400 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 75*GetCastLevel(myHero, _R)+75+0.7*GetBonusAP(myHero) + Ludens()) then 
               KillableEnemies = KillableEnemies + 1
               end
 		  
@@ -171,13 +150,13 @@ OnLoop(function(myHero)
 	      end
 	    end
 		
-	    if CanUseSpell(myHero, _W) == READY and OriannaMenu.Killsteal.W:Value() and GoS:ValidTarget(target, 1200) and GoS:GetDistance(Ball, enemy) <= 250 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 45*GetCastLevel(myHero,_W)+25+0.7*GetBonusAP(myHero) + ExtraDmg) then
+	    if CanUseSpell(myHero, _W) == READY and OriannaMenu.Killsteal.W:Value() and GoS:ValidTarget(target, 1200) and GoS:GetDistance(Ball, enemy) <= 250 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 45*GetCastLevel(myHero,_W)+25+0.7*GetBonusAP(myHero) + Ludens()) then
 	    CastSpell(_W)
-	    elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 825) and OriannaMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 30*GetCastLevel(myHero, _Q)+30+0.5*GetBonusAP(myHero) + ExtraDmg) then 
+	    elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(enemy, 825) and OriannaMenu.Killsteal.Q:Value() and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 30*GetCastLevel(myHero, _Q)+30+0.5*GetBonusAP(myHero) + Ludens()) then 
             CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
             elseif Ball ~= myHero and CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(enemy, 1000) and OriannaMenu.Killsteal.E:Value() then
               local pointSegment,pointLine,isOnSegment  = VectorPointProjectionOnLineSegment(GoS:myHeroPos(), enemyPos, Vector(Ball))
-              if pointLine and GoS:GetDistance(pointSegment, enemy) <= 80 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 30*GetCastLevel(myHero, _R)+30+0.3*GetBonusAP(myHero) + ExtraDmg) then
+              if pointLine and GoS:GetDistance(pointSegment, enemy) <= 80 and GetCurrentHP(enemy)+GetMagicShield(enemy)+GetDmgShield(enemy) < GoS:CalcDamage(myHero, enemy, 0, 30*GetCastLevel(myHero, _R)+30+0.3*GetBonusAP(myHero) + Ludens()) then
               CastTargetSpell(myHero, _E)
               end 
 	    end
@@ -223,33 +202,21 @@ if OriannaMenu.Misc.Autolvl:Value() then
     elseif OriannaMenu.Misc.Autolvltable:Value() == 2 then leveltable = {_Q, _W, _E, _W, _W, _R, _W, _Q, _W, _Q, _R, _Q, _Q, _E, _E, _R, _E, _E}
     elseif OriannaMenu.Misc.Autolvltable:Value() == 3 then leveltable = {_Q, _W, _E, _Q, _Q, _R, _Q, _E, _Q, _E, _R, _E, _E, _W, _W, _R, _W, _W}
     end
-LevelSpell(leveltable[GetLevel(myHero)])
+GoS:DelayAction(function() LevelSpell(leveltable[GetLevel(myHero)]) end, math.random(1000,3000))
 end
-
-if OriannaMenu.Drawings.Ball:Value() then DrawCircle(GetOrigin(Ball).x, GetOrigin(Ball).y, GetOrigin(Ball).z,150,1,128,0xffffffff) end
-if OriannaMenu.Drawings.W:Value() then DrawCircle(GetOrigin(Ball).x, GetOrigin(Ball).y, GetOrigin(Ball).z,250,1,128,0xffffffff) end
-if OriannaMenu.Drawings.R:Value() then DrawCircle(GetOrigin(Ball).x, GetOrigin(Ball).y, GetOrigin(Ball).z,400,1,128,0xffffffff) end
-if OriannaMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,825,1,128,0xff00ff00) end
-if OriannaMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,1000,1,128,0xff00ff00) end
 
 end)
 
 OnProcessSpell(function(unit, spell)
-    if unit and spell and spell.name then
-      if unit == myHero then
-	if spell.name:lower():find("orianaredactcommand") then 
-	Ball = spell.target
-	end
-      end
-    end
+  if unit == myHero and spell.name:lower():find("orianaredactcommand") then 
+  Ball = spell.target
+  end
 end)
 
 OnCreateObj(function(Object) 
-
-if GetObjectBaseName(Object) == "Orianna_Base_Q_yomu_ring_green.troy" then
-Ball = Object
-end
-
+  if GetObjectBaseName(Object) == "Orianna_Base_Q_yomu_ring_green.troy" then
+  Ball = Object
+  end
 end)
 
 OnUpdateBuff(function(Object,buffProc)
@@ -258,72 +225,8 @@ OnUpdateBuff(function(Object,buffProc)
   end
 end)
 
-addInterrupterCallback(function(target, spellType)
+--[[addInterrupterCallback(function(target, spellType)
   if CanUseSpell(myHero, _R) == READY and GoS:GetDistance(Ball, enemy) <= 400 and OriannaMenu.Misc.Interrupt:Value() and spellType == CHANELLING_SPELLS then
   CastSpell(_R)
   end
-end)
-
--- Huge Credits To Inferno for MEC
-local GetOrigin = GetOrigin
-local SQRT = math.sqrt
-
-function TargetDist(point, target)
-    local origin = GetOrigin(target)
-    local dx, dz = origin.x-point.x, origin.z-point.z
-    return SQRT( dx*dx + dz*dz )
-end
-
-function ExcludeFurthest(point, tbl)
-    local removalId = 1
-    for i=2, #tbl do
-        if TargetDist(point, tbl[i]) > TargetDist(point, tbl[removalId]) then
-            removalId = i
-        end
-    end
-    
-    local newTable = {}
-    for i=1, #tbl do
-        if i ~= removalId then
-            newTable[#newTable+1] = tbl[i]
-        end
-    end
-    return newTable
-end
-
-function GetMEC(aoe_radius, listOfEntities, starTarget)
-    local average = {x=0, y=0, z=0, count = 0}
-    for i=1, #listOfEntities do
-        local ori = GetOrigin(listOfEntities[i])
-        average.x = average.x + ori.x
-        average.y = average.y + ori.y
-        average.z = average.z + ori.z
-        average.count = average.count + 1
-    end
-    if starTarget then
-        local ori = GetOrigin(starTarget)
-        average.x = average.x + ori.x
-        average.y = average.y + ori.y
-        average.z = average.z + ori.z
-        average.count = average.count + 1
-    end
-    average.x = average.x / average.count
-    average.y = average.y / average.count
-    average.z = average.z / average.count
-    
-    local targetsInRange = 0
-    for i=1, #listOfEntities do
-        if TargetDist(average, listOfEntities[i]) <= aoe_radius then
-            targetsInRange = targetsInRange + 1
-        end
-    end
-    if starTarget and TargetDist(average, starTarget) <= aoe_radius then
-        targetsInRange = targetsInRange + 1
-    end
-    
-    if targetsInRange == average.count then
-        return average
-    else
-        return GetMEC(aoe_radius, ExcludeFurthest(average, listOfEntities), starTarget)
-    end
-end
+end)]]
